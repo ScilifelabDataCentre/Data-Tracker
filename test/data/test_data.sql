2b8b596bcf29f81c7a6ce7a15f47166689110212
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-09-25 08:09:19 +0200

    Add simple test data.

---
+++ test/data/test_data.sql
@@ -0,0 +1,75 @@
+INSERT INTO users.users (given_name, email, affiliation, country, auth_identity) VALUES ('A Name', 'user1@example.com', 'A University', 'A Country', 'user1auth');
+INSERT INTO users.users (given_name, email, affiliation, country, auth_identity) VALUES ('A Name', 'user2@example.com', 'A University', 'A Country', 'user2auth');
+INSERT INTO users.users (given_name, email, affiliation, country, auth_identity) VALUES ('A Name', 'user3@example.com', 'A University', 'A Country', 'user3auth');
+INSERT INTO users.users (given_name, email, affiliation, country, auth_identity) VALUES ('A Name', 'user4@example.com', 'A University', 'A Country', 'user4auth');
+INSERT INTO users.users (given_name, email, affiliation, country, auth_identity) VALUES ('A Name', 'user5@example.com', 'A University', 'A Country', 'user5auth');
+INSERT INTO users.users (given_name, email, affiliation, country, auth_identity) VALUES ('A Name', 'user6@example.com', 'A University', 'A Country', 'user6auth');
+
+INSERT INTO datasets.datasets (title, description, doi, creator, publication, contact, dmp) VALUES ('Dataset title 1', 'Dataset 1 description', 'doi.portal.1', 'A facility 1', Null, 'contact1@example.com', 'dmp url 1');
+INSERT INTO datasets.datasets (title, description, doi, creator, publication, contact, dmp) VALUES ('Dataset title 2', 'Dataset 2 description', 'doi.portal.2', 'A facility 2', 'Publication 2', 'contact2@example.com', Null);
+INSERT INTO datasets.datasets (title, description, doi, creator, publication, contact, dmp) VALUES ('Dataset title 3', 'Dataset 3 description', 'doi.portal.3', 'A facility 3', Null, 'contact3@example.com', 'dmp url 3');
+INSERT INTO datasets.datasets (title, description, doi, creator, publication, contact, dmp) VALUES ('Dataset title 4', 'Dataset 4 description', 'doi.portal.4', 'A facility 4', Null, 'contact4@example.com', 'dmp url 4');
+INSERT INTO datasets.datasets (title, description, doi, creator, publication, contact, dmp) VALUES ('Dataset title 5', 'Dataset 5 description', 'doi.portal.5', 'A facility 5', 'Publication 5', 'contact5@example.com', Null);
+INSERT INTO datasets.datasets (title, description, doi, creator, publication, contact, dmp) VALUES ('Dataset title 6', 'Dataset 6 description', 'doi.portal.6', 'A facility 6', Null, 'contact6@example.com', Null);
+
+INSERT INTO users.dataset_user_map VALUES (1, 1);
+INSERT INTO users.dataset_user_map VALUES (1, 2);
+INSERT INTO users.dataset_user_map VALUES (2, 2);
+INSERT INTO users.dataset_user_map VALUES (3, 3);
+INSERT INTO users.dataset_user_map VALUES (3, 4);
+INSERT INTO users.dataset_user_map VALUES (4, 5);
+INSERT INTO users.dataset_user_map VALUES (5, 6);
+INSERT INTO users.dataset_user_map VALUES (6, 4);
+
+INSERT INTO datasets.tags (title) VALUES ('Tag Title 1');
+INSERT INTO datasets.tags (title) VALUES ('Tag Title 2');
+INSERT INTO datasets.tags (title) VALUES ('Tag Title 3');
+INSERT INTO datasets.tags (title) VALUES ('Tag Title 4');
+INSERT INTO datasets.tags (title) VALUES ('Tag Title 5');
+INSERT INTO datasets.tags (title) VALUES ('Tag Title 6');
+INSERT INTO datasets.tags (title) VALUES ('Tag Title 7');
+INSERT INTO datasets.tags (title) VALUES ('Tag Title 8');
+INSERT INTO datasets.tags (title) VALUES ('Tag Title 9');
+INSERT INTO datasets.tags (title) VALUES ('Tag Title 10');
+
+INSERT INTO datasets.dataset_tag_map VALUES (1, 2);
+INSERT INTO datasets.dataset_tag_map VALUES (1, 3);
+INSERT INTO datasets.dataset_tag_map VALUES (1, 4);
+INSERT INTO datasets.dataset_tag_map VALUES (1, 5);
+INSERT INTO datasets.dataset_tag_map VALUES (1, 8);
+INSERT INTO datasets.dataset_tag_map VALUES (2, 1);
+INSERT INTO datasets.dataset_tag_map VALUES (2, 3);
+INSERT INTO datasets.dataset_tag_map VALUES (3, 4);
+INSERT INTO datasets.dataset_tag_map VALUES (4, 5);
+INSERT INTO datasets.dataset_tag_map VALUES (4, 7);
+INSERT INTO datasets.dataset_tag_map VALUES (5, 8);
+INSERT INTO datasets.dataset_tag_map VALUES (5, 1);
+INSERT INTO datasets.dataset_tag_map VALUES (5, 6);
+INSERT INTO datasets.dataset_tag_map VALUES (6, 7);
+INSERT INTO datasets.dataset_tag_map VALUES (6, 9);
+INSERT INTO datasets.dataset_tag_map VALUES (6, 10);
+
+INSERT INTO datasets.data_urls (url, description) VALUES ('https:www.example.com/url1', 'URL description 1');
+INSERT INTO datasets.data_urls (url, description) VALUES ('https:www.example.com/url2', 'URL description 2');
+INSERT INTO datasets.data_urls (url, description) VALUES ('https:www.example.com/url3', 'URL description 3');
+INSERT INTO datasets.data_urls (url, description) VALUES ('https:www.example.com/url4', 'URL description 4');
+INSERT INTO datasets.data_urls (url, description) VALUES ('https:www.example.com/url5', 'URL description 5');
+INSERT INTO datasets.data_urls (url, description) VALUES ('https:www.example.com/url6', 'URL description 6');
+INSERT INTO datasets.data_urls (url, description) VALUES ('https:www.example.com/url7', 'URL description 7');
+INSERT INTO datasets.data_urls (url, description) VALUES ('https:www.example.com/url8', 'URL description 8');
+INSERT INTO datasets.data_urls (url, description) VALUES ('https:www.example.com/url9', 'URL description 9');
+INSERT INTO datasets.data_urls (url, description) VALUES ('https:www.example.com/url10', 'URL description 10');
+INSERT INTO datasets.data_urls (url, description) VALUES ('https:www.example.com/url11', 'URL description 11');
+
+INSERT INTO datasets.dataset_data_url_map VALUES (1,1);
+INSERT INTO datasets.dataset_data_url_map VALUES (1,2);
+INSERT INTO datasets.dataset_data_url_map VALUES (2,3);
+INSERT INTO datasets.dataset_data_url_map VALUES (3,3);
+INSERT INTO datasets.dataset_data_url_map VALUES (3,4);
+INSERT INTO datasets.dataset_data_url_map VALUES (4,5);
+INSERT INTO datasets.dataset_data_url_map VALUES (4,6);
+INSERT INTO datasets.dataset_data_url_map VALUES (4,7);
+INSERT INTO datasets.dataset_data_url_map VALUES (5,8);
+INSERT INTO datasets.dataset_data_url_map VALUES (5,9);
+INSERT INTO datasets.dataset_data_url_map VALUES (5,10);
+INSERT INTO datasets.dataset_data_url_map VALUES (6,11);
