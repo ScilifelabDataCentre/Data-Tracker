8716b00792798f89ded08199d2a5896580726fb9
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-10-16 09:45:23 +0200

    Add ProjectInfo component.

---
+++ frontend/src/components/ProjectInfo.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="project-info">
+  {{ projectInfo }}
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'ProjectInfo',
+  props: ['id'],
+  data () {
+    return {
+      projectInfo: null,
+      errorCode: null,
+      errorText: null
+    }
+  },
+  created () {
+    axios
+      .get('http://localhost:5000/api/dataset/' + this.id)
+      .then((response) => (this.projectInfo = response.data))
+      .catch(function (err) {this.errorCode = err.status;
+                             this.errorText = err.statusText});
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
