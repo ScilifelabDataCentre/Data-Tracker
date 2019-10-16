8653e3ed7ec02d45b6430a248a9360a9615e3136
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-10-16 09:48:21 +0200

    Prepare frontend for router.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <router-view></router-view>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: 'App',
+  data() {
+    return {
+      datasets: null
+    }
   }
 }
 </script>
