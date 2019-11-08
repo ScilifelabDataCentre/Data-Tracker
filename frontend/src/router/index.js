5b98ddf85d90a4d25278594de4d7d2f30bacc8ed
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-11-08 09:57:06 +0100

    Updated site structure.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import ProjectAbout from '../components/projectInfo/ProjectAbout.vue'
-import ProjectBrowser from '../components/ProjectBrowser.vue'
-import ProjectContainer from '../components/projectInfo/ProjectContainer.vue'
+import ProjectAbout from '../components/projects/ProjectAbout.vue'
+import ProjectBrowser from '../components/projects/ProjectBrowser.vue'
+import ProjectContainer from '../components/projects/ProjectContainer.vue'
+import ProjectViewer from '../components/projects/ProjectContainer.vue'
+import DatasetAbout from '../components/projects/ProjectAbout.vue'
+import DatasetBrowser from '../components/projects/ProjectBrowser.vue'
+import DatasetContainer from '../components/projects/ProjectContainer.vue'
+import DatasetViewer from '../components/projects/ProjectContainer.vue'
 import AdminUserBrowser from '../components/AdminUserBrowser.vue'
 import NotFound from '../components/NotFound.vue'
 import StartPage from '../components/StartPage.vue'
@@ -14,35 +19,119 @@ const router = new VueRouter({
   base: '/',
   routes: [
     {
-      path: '/projects',
-      component: ProjectBrowser,
-    },
-    {
-      path: '/project/:id/',
-      component: ProjectContainer,
-      props: true,
-      children: [
-        {
-          path: '',
-          redirect: 'about',
-        },
-        {
-          path: 'about',
-          component: ProjectAbout,
-        },
-        {
-          path: 'edit',
-          component: ProjectAbout,
-        }
-      ],
-    },
-    {
       path: '/',
       component: StartPage,
     },
     {
-      path: '/admin/users',
+      path: '/project',
+      component: ProjectContainer,
+      children: [
+        {
+          path: '',
+          redirect: 'browser',
+        },
+        {
+          path: 'browser',
+          component: ProjectBrowser
+        },
+        {
+          path: ':id',
+          component: ProjectViewer,
+          props: true,
+          children: [
+            {
+              path: '',
+              redirect: 'about',
+            },
+            {
+              path: 'about',
+              component: ProjectAbout,
+            },
+            {
+              path: 'edit',
+              component: ProjectAbout,
+            }
+          ],
+        }
+      ],
+    },
+    {
+      path: '/dataset',
+      component: DatasetContainer,
+      children: [
+        {
+          path: '',
+          redirect: 'browser',
+        },
+        {
+          path: 'browser',
+          component: DatasetBrowser
+        },
+        {
+          path: ':id',
+          component: DatasetViewer,
+          props: true,
+          children: [
+            {
+              path: '',
+              redirect: 'about',
+            },
+            {
+              path: 'about',
+              component: DatasetAbout,
+            },
+            {
+              path: 'edit',
+              component: DatasetAbout,
+            }
+          ],
+        }
+      ],
+    },
+    {
+      path: '/user',
       component: AdminUserBrowser,
+      children: [   
+        {
+          path: '',
+          redirect: 'about'
+        },
+        {
+          path: 'about',
+          component: AdminUserBrowser,
+        },
+        {
+          path: 'edit',
+          component: AdminUserBrowser,
+        },
+      ],
+    },
+    {
+      path: '/admin',
+      component: AdminUserBrowser,
+      children: [
+        {
+          path: '',
+          redirect: 'stats',
+        },
+
+        {
+          path: 'stats',
+          component: AdminUserBrowser,
+        },
+        {
+          path: 'projects',
+          component: AdminUserBrowser,
+        },
+        {
+          path: 'datasets',
+          component: AdminUserBrowser,
+        },
+        {
+          path: 'users',
+          component: AdminUserBrowser,
+        }
+      ],
     },
     {
       path: '*',
