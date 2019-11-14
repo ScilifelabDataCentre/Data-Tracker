c7ba4f7716c11d37b878b7f335e542ae9729c454
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-11-14 08:51:07 +0100

    Add image for home in navbar, make admin a dropdown menu.

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -1,15 +1,19 @@
 <template>
 <nav class="navbar" role="navigation" aria-label="main navigation">
-  <router-link to="/" class="navbar-item"><span>Home</span></router-link>
-  <router-link to="/project" class="navbar-item"><span>Projects</span></router-link>
-  <router-link to="/dataset" class="navbar-item"><span>Datasets</span></router-link>
-  <router-link to="/about" class="navbar-item"><span>About</span></router-link>
-  <router-link v-if="user.permission === 'Admin'" to="/admin" class="navbar-item"><span>Admin</span></router-link>
+  <router-link to="/" class="navbar-item"><img id="home-icon" :src="require('../assets/open-iconic/svg/home.svg')" alt="home"></router-link>
+  <router-link to="/project" class="navbar-item">Projects</router-link>
+  <router-link to="/dataset" class="navbar-item">Datasets</router-link>
+  <router-link to="/about" class="navbar-item">About us</router-link>
+  <div v-if="user.permission === 'Admin'" class="navbar-item has-dropdown is-hoverable">
+    <a class="navbar-link">Admin</a>
+    <div class="navbar-dropdown">
+      <router-link to="/admin/users" class="navbar-item">User management</router-link>
+      <router-link to="/admin/stats" class="navbar-item">Statistics</router-link>
+    </div>
+  </div>
   <div class="navbar-end">
     <div v-if="user.user" class="navbar-item has-dropdown is-hoverable">
-      <a class="navbar-link">
-        <span>{{ user.user }}</span>
-      </a>
+      <a class="navbar-link">{{ user.user }}</a>
       <div class="navbar-dropdown">
         <router-link v-if="user.user" to="/user" class="navbar-item">Info</router-link>
         <a v-if="user.user" href="/logout" class="navbar-item">Logout</a>
@@ -36,9 +40,8 @@ export default {
 </script>
 
 <style scoped>
-.navigation-bar {
-    padding: 5px 0px}
-.navigation-link {
-    padding: 0px 10px;
+#home-icon {
+    height:12px;
+    width:12px;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router';
 import store from './store';
 
-import './../node_modules/bulma/css/bulma.css';
+import '../node_modules/bulma/css/bulma.css';
 
 Vue.config.productionTip = false
 
