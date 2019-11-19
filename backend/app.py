0a59aa1503a2fd414396dbc87ed4d011fba6d65c
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2019-11-19 10:30:43 +0100

    Read settings to app.

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,24 @@
+import logging
+
 import flask
 
+from .settings import SETTINGS
+
 app = flask.Flask('backend')
+
+if SETTINGS['development_mode']:
+    logging.getLogger().setLevel(logging.DEBUG)
+    app.config['TESTING'] = True
+
+app.config['SECRET_KEY'] = SETTINGS['flask']['flask_secret']
 
 @app.route('/api/hello')
 def api_hello():
     return flask.jsonify({'test': 'value'})
+
+def list_datasets():
+    """
+    Provide a simplified list of all available datasets.
+    """
+
+    return flask.jsonify()
