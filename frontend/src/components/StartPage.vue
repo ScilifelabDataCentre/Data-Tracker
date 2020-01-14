5b54b3551a25cac77d17ec1caf4c057801ba2321
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-01-14 08:37:15 +0100

    Add random project to start page. Column design with random dataset.

--- frontend/src/components/StartPage.vue
+++ frontend/src/components/StartPage.vue
@@ -3,17 +3,26 @@
   <section class="section">
     Welcome to the SciLifeLab Data Tracker, a system for tracking datasets generated at SciLifeLab.
   </section>
-    <section class="section" v-if="random_ds">
+  <div class="columns">
+    <div class = "column" v-if="random_proj">
+      <h4 class="title is-4">
+        Random Project
+      </h4>
+      <proj_entry :project="random_proj.projects[0]"></proj_entry>
+    </div>
+    <div class = "column" v-if="random_ds">
       <h4 class="title is-4">
         Random Dataset
       </h4>
-      <ds_entry :dataset="random_ds.dataset[0]"></ds_entry>
-    </section>
+      <ds_entry :dataset="random_ds.datasets[0]"></ds_entry>
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
 import DatasetEntry from './datasets/DatasetEntry.vue';
+import ProjectEntry from './projects/ProjectEntry.vue';
 import axios from 'axios';
 
 export default {
@@ -22,11 +31,13 @@ export default {
   data () {
     return {
       random_ds: null,
+      random_proj: null,
     }
   },
 
   components: {
     'ds_entry': DatasetEntry,
+    'proj_entry': ProjectEntry,
   },
 
   mounted () {
@@ -34,7 +45,13 @@ export default {
       .get('/api/dataset/random')
       .then((response) => {            
         this.random_ds = response.data;
-      })
+      });
+    axios
+      .get('/api/project/random')
+      .then((response) => {            
+        this.random_proj = response.data;
+      });
+
   },
 }
 </script>
