ede0e4e94f79bbf69ad5873b3912461a05ce7e4d
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-01-14 20:44:02 +0100

    Remove unused files.

--- backend/portal_errors.py
+++
@@ -1,4 +0,0 @@
-"""Custom errors used in the portal."""
-
-class InsufficientPermissions(Exception):
-    """The current user does not have the required permissions to perform the requested action."""
