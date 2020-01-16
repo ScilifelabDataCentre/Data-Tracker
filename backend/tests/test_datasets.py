ec804b929db9e4d52a64ecbef4dcf6d150f61eff
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-01-16 07:36:36 +0100

    Add more tests for dataset requests.

--- backend/tests/test_datasets.py
+++ backend/tests/test_datasets.py
@@ -8,6 +8,38 @@ import helpers
 # pylint: disable=redefined-outer-name
 
 
+def test_list_datasets_get():
+    """
+    Request a list of all datasets.
+
+    Should also test e.g. pagination once implemented.
+    """
+    responses = helpers.make_request_all_roles('/api/dataset/all')
+    assert [response[1] for response in responses] == [200, 200, 200, 200]
+    assert [len(json.loads(response[0])['datasets']) > 10 for response in responses] == [True]*4
+
+
+def test_random_dataset_get():
+    """Request a random dataset."""
+    responses = helpers.make_request_all_roles('/api/dataset/random')
+    assert [response[1] for response in responses] == [200, 200, 200, 200]
+    assert [len(json.loads(response[0])['datasets']) == 1 for response in responses] == [True]*4
+
+
+def test_random_datasets_get():
+    """Request random datasets."""
+    session = requests.Session()
+    helpers.as_user(session, helpers.USERS['user'])
+    for i in (1, 5, 0):
+        response = helpers.make_request(session, f'/api/dataset/random/{i}')
+        assert response[1] == 200
+        assert len(response[0]['datasets']) == i
+
+    response = helpers.make_request(session, '/api/dataset/random/-1')
+    assert response[1] == 404
+    assert not response[0]
+
+
 def test_add_get():
     """
     Request data structure for .get(dataset/add).
