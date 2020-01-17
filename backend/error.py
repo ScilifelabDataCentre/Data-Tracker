06501a71f29fedff4766bc58895ba08cc7b872bc
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-01-17 07:51:40 +0100

    Add custom exception for updates trying to update forbidden fields.

--- backend/error.py
+++ backend/error.py
@@ -3,3 +3,7 @@
 
 class BadIdentification(Exception):
     """Bad user identifier (username/API key)."""
+
+
+class ForbiddenUpdateInput(Exception):
+    """The input contained fields that may not be updated manually."""
