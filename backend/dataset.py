ca5dba8a439db7abd1f33d6bd10a5f8e0fcd432a
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-01-18 10:05:51 +0100

    Make sure random and get returns the same data structure. Add initial implementation of update.

--- backend/dataset.py
+++ backend/dataset.py
@@ -1,5 +1,4 @@
 """Dataset requests."""
-
 import flask
 
 import structure
@@ -8,6 +7,28 @@ import user
 
 
 blueprint = flask.Blueprint('datasets', __name__)  # pylint: disable=invalid-name
+
+def query_dataset(identifier: str):
+    """
+    Query for a dataset from the database.
+
+    Args:
+        identifier (str): the uuid of the dataset
+
+    Returns:
+        dict: the dataset
+
+    """
+    try:
+        mongo_uuid = utils.to_mongo_uuid(identifier)
+        result = flask.g.db['datasets'].find_one({'uuid': mongo_uuid})
+        result['projects'] = [{'title': hit['title'], 'uuid': hit['uuid']}
+                              for hit in flask.g.db['projects'].find({'datasets': result['uuid']})]
+        utils.clean_mongo(result)
+        utils.clean_mongo(result['projects'])
+    except ValueError:
+        result = None
+    return result
 
 
 @blueprint.route('/all', methods=['GET'])
@@ -48,11 +69,12 @@ def get_random_ds(amount: int = 1):
         amount (int): number of requested datasets
 
     Returns:
-        flask.Request: json structure for the dataset(s)
+        flask.Response: json structure for the dataset(s)
 
     """
     results = list(flask.g.db['datasets'].aggregate([{'$sample': {'size': amount}}]))
-    utils.clean_mongo(results)
+    for i, result in enumerate(results):
+        results[i] = query_dataset(result['uuid'].hex)
     return utils.response_json({'datasets': results})
 
 
@@ -65,20 +87,12 @@ def get_dataset(identifier):
         identifier (str): uuid for the wanted dataset
 
     Returns:
-        flask.Request: json structure for the dataset
+        flask.Response: json structure for the dataset
 
     """
-    try:
-        mongo_uuid = utils.to_mongo_uuid(identifier)
-        result = flask.g.db['datasets'].find_one({'uuid': mongo_uuid})
-    except ValueError:
-        result = None
-
+    result = query_dataset(identifier)
     if not result:
         return flask.Response(status=404)
-    result['projects'] = [{'title': hit['title'], 'uuid': hit['uuid']}
-                          for hit in flask.g.db['projects'].find({'datasets': result['uuid']})]
-    utils.clean_mongo(result)
     return utils.response_json({'dataset': result})
 
 
@@ -94,3 +108,30 @@ def delete_dataset(identifier):
     if result.deleted_count == 0:
         return flask.Response(status=404)
     return flask.Response(status=200)
+
+
+@blueprint.route('/<identifier>', methods=['PUT'])
+@user.login_required
+# require Steward or owning dataset
+def update_dataset(identifier):
+    """
+    Update a dataset with new values.
+
+    Args:
+        identifier (str): uuid for the wanted dataset
+
+    Returns:
+        flask.Response: success: 200, failure: 400
+
+    """
+    if not utils.check_user_permissions('Steward'):
+        current = flask.g.g.db['datasets'].find_one({'uuid': identifier})
+        if not utils.is_owner(dataset=current):
+            flask.abort(flask.Response(status=403))
+
+    data = flask.request.json
+    try:
+        utils.check_mongo_update(data)
+    except ValueError:
+        flask.abort(flask.Response(status=400))
+    #    flask.g.db.datasets.update()
