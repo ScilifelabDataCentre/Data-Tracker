294faf7c60328e415a5d3623855fcfae4bacaa04
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-01-24 13:41:34 +0100

    Remove unused errors.

--- backend/error.py
+++
@@ -1,9 +0,0 @@
-"""Custom exceptions."""
-
-
-class BadIdentification(Exception):
-    """Bad user identifier (username/API key)."""
-
-
-class ForbiddenUpdateInput(Exception):
-    """The input contained fields that may not be updated manually."""
