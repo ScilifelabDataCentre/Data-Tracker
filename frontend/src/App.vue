b20cefb08de4a42b09533faf698c1ae491326321
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-01-24 14:17:12 +0100

    Add notification bar.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,13 @@
 <template>
 <div id="app">
   <nav-bar></nav-bar>
+
+  <transition name="notification-fade">
+    <div v-if="notification" :class="{notification: true, 'is-info': !error, 'is-danger': error}">
+      {{ notification }}
+    </div>
+  </transition>
+
   <router-view class="container"></router-view>
   <footer class="footer">
     <img class="logo" :src="require('./assets/img/data-centre-logo.png')" alt="SciLife Data Centre logo"/>
@@ -9,6 +16,7 @@
 </template>
 
 <script>
+import {mapGetters} from 'vuex';
 import NavigationBar from './components/NavigationBar.vue';
 
 export default {
@@ -16,10 +24,14 @@ export default {
   components: {
     'nav-bar': NavigationBar
   },
+  computed: {
+    ...mapGetters(['datasets', 'user']),
+  },
   data() {
     return {
     }
   },
+  
   created () {
     this.$store.dispatch('getUser');
   },
@@ -31,9 +43,18 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
+
 img.logo {
     height: 70px;
 }
+
+.notification-fade-enter-active, .notification-fade-leave-active {
+    transition: opacity .5s;
+}
+
+.notification-fade-enter, .notification-fade-leave-to {
+    opacity: 0;
+}
+
 </style>
