87092c22bd04b0f081b44a4956e2507322cfe4d2
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-01-26 20:58:36 +0100

    Add skeleton to show off ideas.

--- frontend/src/components/user/UserAbout.vue
+++ frontend/src/components/user/UserAbout.vue
@@ -1,16 +1,52 @@
 <template>
 <div class="user-about">
-  <div v-if="!user.user">
+  <div v-if="!user.email">
     You need to log in before viewing this page.
   </div>
   <div v-else>
-    <table class="table">
-      <tr><td class="data-header">Name</td> <td>{{user.user}}</td></tr>
-      <tr><td class="data-header">Affiliation</td><td>{{user.affiliation}}</td></tr>
-      <tr><td class="data-header">Email</td> <td>{{user.email}}</td></tr>
-      <tr><td class="data-header">Country</td> <td>{{user.country}}</td></tr>
-      <tr><td class="data-header">User type</td> <td>{{user.permission}}</td></tr>
+    <table class="table is-hoverable">
+      <tbody>
+        <tr><td class="data-header">Name</td> <td>{{user.name}}</td></tr>
+        <tr><td class="data-header">Affiliation</td><td>{{user.affiliation}}</td></tr>
+        <tr><td class="data-header">Email</td> <td>{{user.email}}</td></tr>
+        <tr><td class="data-header">Country</td> <td>{{user.country}}</td></tr>
+        <tr><td class="data-header">User type</td> <td>{{user.role}}</td></tr>
+      </tbody>
     </table>
+
+    <div>
+      <table class="table is-hoverable is-striped is-fullwidth">
+        <thead>
+          <tr><th>Title</th><th>Something else</th></tr>
+        </thead>
+        <tbody>
+          <tr><td>List of datasets received by user (order/receiver)</td><td>Some info</td></tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div>
+      <table class="table is-hoverable is-striped is-fullwidth">
+        <thead>
+          <tr><th>Title</th><th>Something else</th></tr>
+        </thead>
+        <tbody>
+          <tr><td>List of orders owned by user (creator)</td><td>Some info</td></tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div>
+      <table class="table is-hoverable is-striped is-fullwidth">
+        <thead>
+          <tr><th>Title</th><th>Something else</th></tr>
+        </thead>
+        <tbody>
+          <tr><td>List of projects owned by user</td><td>Some info</td></tr>
+        </tbody>
+      </table>
+    </div>
+
   </div>
 </div>
 </template>
