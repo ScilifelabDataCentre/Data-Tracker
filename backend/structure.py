dfc1df517ce5503d9ad8d838778f1cff64cf0931
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-02-03 08:27:52 +0100

    Update data structures
    Add order structure and a validator for it. Remove fields will now be unused.

--- backend/structure.py
+++ backend/structure.py
@@ -1,7 +1,5 @@
 """Required fields for the different data types."""
 
-import datetime
-
 import utils
 
 
@@ -17,8 +15,7 @@ def user():
             'country': '',
             'email': '',
             'name': '',
-            'role': 'User',
-            'timestamp': datetime.datetime.now()}
+            'role': 'User'}
 
 
 def dataset():
@@ -28,15 +25,50 @@ def dataset():
     Returns:
         dict: the data structure for datasets
     """
-    return {'creator': '',
-            'data_urls': [],
+    return {'data_urls': [],
             'description': '',
-            'dmp': '',
             'identifier': '',
-            'publications': [],
-            'timestamp': datetime.datetime.now(),
             'title': '',
-            'uuid': utils.new_uuid()}
+            '_id': utils.new_uuid()}
+
+
+def order():
+    """
+    Provide a basic data structure for an order.
+
+    Returns:
+        dict: the data structure for orders
+    """
+    return {'creator': '',
+            'receiver': '',
+            'description': '',
+            'title': '',
+            '_id': utils.new_uuid()}
+
+
+def order_validator(order: dict):
+    """
+    Validate the content of the fields of an incoming order.
+
+    Args:
+        order (dict): order to check
+
+    Raises:
+        ValueError: bad incoming data
+
+    """
+    expected = order()
+    if set(in_order.keys()) - set(expected.keys()):
+        raise ValueError('Unexpected fields in input')
+
+    if not utils.is_email(in_order['creator']) or '-is-facility-':
+        raise ValueError('Creator should be a user (email) or a facility')
+
+    if not utils.is_email(in_order['receiver']):
+        raise ValueError('Receiver should be a user (email)')
+
+    if not title:
+        raise ValueError('Title should not be empty')
 
 
 def project():
@@ -46,11 +78,12 @@ def project():
     Returns:
         dict: the data structure for projects
     """
-    return {'contact': '',
+    return {'dmp': '',
+            'contact': '',
             'description': '',
             'identifier': '',
             'owner': '',
-            'timestamp': datetime.datetime.now(),
+            'publications': [],
             'title': '',
             'datasets': [],
-            'uuid': utils.new_uuid()}
+            '_id': utils.new_uuid()}
