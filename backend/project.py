119d51ba773c343d706e31978778a307f0ef1846
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-03-02 14:45:19 +0100

    All tests passing

--- backend/project.py
+++ backend/project.py
@@ -1,4 +1,4 @@
-"""Dataset requests."""
+"""Project requests."""
 import json
 import logging
 
@@ -9,42 +9,10 @@ import utils
 import user
 
 
-blueprint = flask.Blueprint('projects', __name__)  # pylint: disable=invalid-name
+blueprint = flask.Blueprint('project', __name__)  # pylint: disable=invalid-name
 
 
-def validate_project_input(indata):
-    """
-    Validate the project input.
-
-    It may only contain valid fields that may be changed by the role of the current user.
-
-    Args:
-        indata: the project input
-
-    Returns:
-        bool: whether the project input is accepted
-
-    """
-    if not utils.check_mongo_update(indata):
-        return False
-    # check that fields should exist and are not forbidden
-    reference = set(structure.project().keys())
-    forbidden = {'identifier'}
-    inkeys = set(indata.keys())
-    if not inkeys.issubset(reference) or forbidden&inkeys:
-        logging.debug('Bad input: %s', inkeys)
-        return False
-
-    # check restricted (admin/steward) fields
-    restricted_steward = {'owner', 'datasets'}
-    if not user.check_user_permissions('Steward') and restricted_steward&inkeys:
-        logging.debug('Restricted input: %s', inkeys)
-        return False
-
-    return True
-
-
-@blueprint.route('/all', methods=['GET'])
+@blueprint.route('/', methods=['GET'])
 def list_project():
     """Provide a simplified list of all available projects."""
     results = list(flask.g.db['projects'].find())
@@ -89,37 +57,3 @@ def get_project(identifier):
     if not result:
         return flask.Response(status=404)
     return utils.response_json({'project': result})
-
-
-@blueprint.route('/add', methods=['GET'])
-@user.steward_required
-def add_project_get():
-    """Provide a basic data structure for adding a project."""
-    project = structure.project()
-    del project['_id']
-    del project['identifiers']
-    return utils.response_json(project)
-
-
-@blueprint.route('/add', methods=['POST'])
-@user.steward_required
-def add_project_post():
-    """Add a project."""
-    project = structure.project()
-
-    indata = json.loads(flask.request.data)
-    if indata:
-        if not validate_project_input(indata):
-            flask.abort(flask.Response(status=400))
-        project.update(indata)
-
-    if 'datasets' in project:
-        try:
-            project['datasets'] = [utils.str_to_uuid(ds) for ds in project['datasets']]
-        except ValueError:
-            flask.abort(flask.Response(status=400))
-
-    result = flask.g.db['projects'].insert_one(project)
-    entry = flask.g.db['projects'].find_one({'_id': result.inserted_id},
-                                            {'_id': 1})
-    return utils.response_json(entry)
--- backend/tests/test_projects.py
+++ backend/tests/test_projects.py
@@ -4,7 +4,7 @@ import uuid
 import requests
 
 from helpers import make_request, as_user, make_request_all_roles,\
-    USERS, random_string
+    USERS, random_string, use_db
 # pylint: disable=redefined-outer-name
 
 def test_list_projects():
@@ -13,57 +13,62 @@ def test_list_projects():
 
     Should also test e.g. pagination once implemented.
     """
-    responses = make_request_all_roles('/api/project/all')
-    assert [response[1] for response in responses] == [200, 200, 200, 200]
+    responses = make_request_all_roles('/api/project/', ret_json=True)
     for response in responses:
-        assert len(json.loads(response[0])['projects']) == 500
+        assert response.code == 200
+        assert len(response.data['projects']) == 500
 
 
 def test_random_project():
     """Request a random project."""
-    responses = make_request_all_roles('/api/project/random')
-    assert [response[1] for response in responses] == [200, 200, 200, 200]
+    responses = make_request_all_roles('/api/project/random', ret_json=True)
     for response in responses:
-        assert len(json.loads(response[0])['projects']) == 1
+        assert response.code == 200
+        assert len(response.data['projects']) == 1
 
 
 def test_random_projects():
     """Request random projects."""
     session = requests.Session()
-    as_user(session, USERS['user'])
+    as_user(session, USERS['base'])
     for i in (1, 5, 0):
-        response = make_request(session, f'/api/project/random/{i}')
-        assert response[1] == 200
-        assert len(response[0]['projects']) == i
+        response = make_request(session, f'/api/project/random/{i}', ret_json=True)
+        assert response.code == 200
+        assert len(response.data['projects']) == i
 
     response = make_request(session, '/api/project/random/-1')
     assert response[1] == 404
     assert not response[0]
 
 
-def test_get_project_permissions():
+def test_get_project_permissions(use_db):
     """Test permissions for requesting a project."""
-    session = requests.Session()
-    orig = make_request(session, '/api/project/random')[0]['projects'][0]
-    responses = make_request_all_roles(f'/api/project/{orig["_id"]}')
+    db = use_db
+    project = list(db['projects'].aggregate([{'$sample': {'size': 1}}]))[0]
+
+    responses = make_request_all_roles(f'/api/project/{project["_id"]}', ret_json=True)
     for response in responses:
-        assert json.loads(response[0])['project'] == orig
-        assert response[1] == 200
+        assert response.code == 200
 
 
-def test_get_project():
+def test_get_project(use_db):
     """
     Request multiple projects by uuid, one at a time.
 
     Projects are choosen randomly using /api/project/random.
     """
+    db = use_db
     session = requests.Session()
     for _ in range(10):
-        orig = make_request(session, '/api/project/random')[0]['projects'][0]
-        response = make_request(session, f'/api/project/{orig["_id"]}')
-        assert response[1] == 200
-        requested = response[0]['project']
-        assert orig == requested
+        project = list(db['projects'].aggregate([{'$sample': {'size': 1}}]))[0]
+        print(project)
+        project['_id'] = str(project['_id'])
+        project['owners'] = [str(entry) for entry in project['owners']]
+        project['datasets'] = [str(entry) for entry in project['datasets']]
+        response = make_request(session, f'/api/project/{project["_id"]}')
+        assert response.code == 200
+        for field in project:
+            assert project[field] == response.data['project'][field]
 
 
 def test_get_project_bad():
@@ -73,33 +78,12 @@ def test_get_project_bad():
     All are expected to return 404.
     """
     session = requests.Session()
-    for _ in range(5):
+    for _ in range(2):
         response = make_request(session, f'/api/project/{uuid.uuid4().hex}')
-        assert response == (None, 404)
+        assert response.code == 404
+        assert not response.data
 
-    for _ in range(5):
+    for _ in range(2):
         response = make_request(session, f'/api/project/{random_string()}')
-        assert response == (None, 404)
-
-
-def test_add_get():
-    """
-    Request data structure for .get(project/add).
-
-    Should require at least Steward.
-    """
-    expected_success = {'contact': '',
-                        'datasets': [],
-                        'description': '',
-                        'dmp': '',
-                        'owner': '',
-                        'publications': [],
-                        'title': ''}
-
-    responses = make_request_all_roles('/api/project/add')
-    assert [response[1] for response in responses] == [401, 401, 200, 200]
-    assert [json.loads(response[0]) if response[0] else None
-            for response in responses] == [None,
-                                           None,
-                                           expected_success,
-                                           expected_success]
+        assert response.code == 404
+        assert not response.data
