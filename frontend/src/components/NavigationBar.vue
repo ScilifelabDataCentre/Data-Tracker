5966e91e51ab224a645e06a2185a1ceadb1f417f
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-03-05 13:29:53 +0100

    Update navbar to look better on small screens

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -1,35 +1,47 @@
 <template>
 <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-start">
+  <div class="navbar-brand">
     <div class="navbar-item">
       SciLifeLab Data Tracker
     </div>
+    <a role="button"
+       class="navbar-burger"
+       aria-label="menu"
+       aria-expanded="false"
+       @click="showMenu = !showMenu">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
   </div>
-  <router-link to="/" class="navbar-item">
-    <img class="icon-home" :src="require('../assets/open-iconic/svg/home.svg')" alt="Start page" />
-  </router-link>
-  <router-link to="/search" class="navbar-item">Search</router-link>
-  <router-link to="/project" class="navbar-item">Projects</router-link>
-  <router-link  v-if="user.role === 'Admin'" to="/order" class="navbar-item">Orders</router-link>
-  <router-link to="/dataset" class="navbar-item">Datasets</router-link>
-  <router-link to="/about" class="navbar-item">About</router-link>
-  <div v-if="user.role === 'Admin'" class="navbar-item has-dropdown is-hoverable">
-    <a class="navbar-link">Admin</a>
-    <div class="navbar-dropdown">
-      <router-link to="/admin/stats" class="navbar-item">Statistics</router-link>
-      <router-link to="/admin/dois" class="navbar-item">DOI management</router-link>
-      <router-link to="/admin/users" class="navbar-item">User management</router-link>
-    </div>
-  </div>
-  <div class="navbar-end">
-    <div v-if="user.name" class="navbar-item has-dropdown is-hoverable">
-      <a class="navbar-link">{{ user.name }}</a>
+  <div class="navbar-menu" id="navbarMenu" :class="{'navbar-menu': true, 'is-active': showMenu}">
+    <div class="navbar-start"></div>
+    <router-link to="/" class="navbar-item">
+      <img class="icon-home" :src="require('../assets/open-iconic/svg/home.svg')" alt="Start page" />
+    </router-link>
+    <router-link to="/search" class="navbar-item">Search</router-link>
+    <router-link to="/project" class="navbar-item">Projects</router-link>
+    <router-link  v-if="user.role === 'Admin'" to="/order" class="navbar-item">Orders</router-link>
+    <router-link to="/dataset" class="navbar-item">Datasets</router-link>
+    <router-link to="/about" class="navbar-item">About</router-link>
+    <div v-if="user.role === 'Admin'" class="navbar-item has-dropdown is-hoverable">
+      <a class="navbar-link">Admin</a>
       <div class="navbar-dropdown">
-        <router-link to="/user" class="navbar-item">Info</router-link>
-        <a href="/api/user/logout" class="navbar-item">Logout</a>
+        <router-link to="/admin/stats" class="navbar-item">Statistics</router-link>
+        <router-link to="/admin/dois" class="navbar-item">DOI management</router-link>
+        <router-link to="/admin/users" class="navbar-item">User management</router-link>
       </div>
     </div>
-    <a v-else href="/elixir/login" class="navbar-item">Log in</a>
+    <div class="navbar-end">
+      <div v-if="user.name" class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">{{ user.name }}</a>
+        <div class="navbar-dropdown">
+          <router-link to="/user" class="navbar-item">Info</router-link>
+          <a href="/api/user/logout" class="navbar-item">Logout</a>
+        </div>
+      </div>
+      <a v-else href="/elixir/login" class="navbar-item">Log in</a>
+    </div>
   </div>
 </nav>
 </template>
@@ -44,6 +56,7 @@ export default {
   },
   data() {
     return {
+      'showMenu': false,
     }
   }
 }
