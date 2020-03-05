3a83e67c73a642092deedd6a4362405a5b5154bb
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-03-05 22:31:27 +0100

    Update DatasetAbout page for new data structure

--- frontend/src/components/datasets/DatasetAbout.vue
+++ frontend/src/components/datasets/DatasetAbout.vue
@@ -3,15 +3,56 @@
   <div v-if="Object.keys(dataset).length > 0">
     <h1 class="title is-2">{{ dataset.title }}</h1>
     <div class="dataset-description field" v-html="dataset.description"></div>
-    <div class="message">
-      <div class="message-header">
-        Data access
-      </div>
-      <ul v-for="location in dataset.links" :key="location.id">
-        <li class="test"><a :href="location.url">{{location.description}}</a></li>
-      </ul>
-    </div>
-    <span class="timestamp">Last update: {{ dataset.timestamp }}</span>
+    <table class="table is-hoverable is-striped">
+      <tbody>
+        <tr>
+          <th scope="row">
+            Dataset creator
+          </th>
+          <td>
+            {{ dataset.creator }}
+          </td>
+        </tr>
+        <tr v-if="dataset.links.length > 0">
+          <th scope="row">
+            Links
+          </th>
+          <td>
+            <ul v-for="location in dataset.links" :key="location">
+              <li class="test"><a :href="location.url">{{location.description}}</a></li>
+            </ul>
+          </td>
+        </tr>
+        <tr v-if="dataset.projects.length > 0">
+          <th scope="row">
+            Projects
+          </th>
+          <td>
+            <ul v-for="project in dataset.projects" :key="project._id">
+              <li class="test"><a :href="'/project/' + project._id">{{project.title}}</a></li>
+            </ul>
+          </td>
+        </tr>
+        <tr v-if="dataset.related.length > 0">
+          <th scope="row">
+            Related datasets
+          </th>
+          <td>
+            <ul v-for="dataset in dataset.related" :key="dataset._id">
+              <li class="test"><a :href="'/dataset/' + dataset._id">{{dataset.title}}</a></li>
+            </ul>
+          </td>
+        </tr>
+        <tr v-for="extraField in dataset.extras" :key="extraField.key">
+          <th scope="row" :rowspan="user.permissions.length">
+            {{ extraField.key }}
+          </th>
+          <td>
+            {{ extraField.value }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
   <button class="button is-link" @click="editDataset">
     Edit
