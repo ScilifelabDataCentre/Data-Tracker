82387ed4a9cc9fa5923d5c8c856776f163cfc09b
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-03-05 16:04:31 +0100

    Improve 'about user' page

--- frontend/src/components/user/UserAbout.vue
+++ frontend/src/components/user/UserAbout.vue
@@ -1,52 +1,99 @@
 <template>
 <div class="user-about">
-  <div v-if="!user.email">
-    You need to log in before viewing this page.
-  </div>
-  <div v-else>
-    <table class="table is-hoverable">
+  <h4 class="title is-4">User Information</h4>
+  <table class="table is-hoverable is-striped">
+    <tbody>
+      <tr>
+        <th scope="row">Name</th>
+        <td>{{user.name}}</td>
+      </tr>
+      <tr>
+        <th scope="row">Affiliation</th>
+        <td>{{user.affiliation}}</td>
+      </tr>
+      <tr>
+        <th scope="row">Email</th>
+        <td>{{user.email}}</td>
+      </tr>
+      <tr>
+        <th scope="row">Country</th>
+        <td>{{user.country}}</td>
+      </tr>
+      <tr v-if="user.permissions.length > 0">
+        <th scope="row" :rowspan="user.permissions.length">Permissions</th>
+        <td>
+          <ul>
+            <li v-for="entry in user.permissions" :key="entry">
+              <span class="tag is-info is-light">
+                {{entry}}
+              </span>
+            </li>
+          </ul>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+  <div v-if="user_orders.length > 0">
+    <h4 class="title is-4">Orders</h4>
+    <table class="table is-hoverable is-striped">
+      <thead>
+        <tr>
+          <th>UUID</th>
+          <th>Title</th>
+          <th>Datasets</th>
+        </tr>
+      </thead>
       <tbody>
-        <tr><td class="data-header">Name</td> <td>{{user.name}}</td></tr>
-        <tr><td class="data-header">Affiliation</td><td>{{user.affiliation}}</td></tr>
-        <tr><td class="data-header">Email</td> <td>{{user.email}}</td></tr>
-        <tr><td class="data-header">Country</td> <td>{{user.country}}</td></tr>
-        <tr><td class="data-header">User type</td> <td>{{user.role}}</td></tr>
+        <tr v-for="order in user_orders" :key="order._id">
+          <td><a :href="'/dataset/' + order._id">{{ order._id }}</a></td>
+          <td>{{ order.title }}</td>
+          <td>
+            <ul>
+              <li v-for="ds_uuid in order.datasets" :key="ds_uuid">
+                <a :href="'/dataset/' + ds_uuid">{{ ds_uuid }}</a>
+              </li>
+            </ul>
+          </td>
+        </tr>
       </tbody>
     </table>
+  </div>
 
-    <div>
-      <table class="table is-hoverable is-striped is-fullwidth">
-        <thead>
-          <tr><th>Title</th><th>Something else</th></tr>
-        </thead>
-        <tbody>
-          <tr><td>List of datasets received by user (order/receiver)</td><td>Some info</td></tr>
-        </tbody>
-      </table>
-    </div>
+  <div v-if="user_datasets.length > 0">
+    <h4 class="title is-4">Datasets</h4>
+    <table class="table is-hoverable is-striped">
+      <thead>
+        <tr>
+          <th>UUID</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="dataset in user_datasets" :key="dataset._id">
+          <td><a :href="'/dataset/' + dataset._id">{{ dataset._id }}</a></td>
+          <td>{{ dataset.title }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 
-    <div>
-      <table class="table is-hoverable is-striped is-fullwidth">
-        <thead>
-          <tr><th>Title</th><th>Something else</th></tr>
-        </thead>
-        <tbody>
-          <tr><td>List of orders owned by user (creator)</td><td>Some info</td></tr>
-        </tbody>
-      </table>
-    </div>
-
-    <div>
-      <table class="table is-hoverable is-striped is-fullwidth">
-        <thead>
-          <tr><th>Title</th><th>Something else</th></tr>
-        </thead>
-        <tbody>
-          <tr><td>List of projects owned by user</td><td>Some info</td></tr>
-        </tbody>
-      </table>
-    </div>
-
+  <div v-if="user_projects.length > 0">
+    <h4 class="title is-4">Projects</h4>
+    <table class="table is-hoverable is-striped is-fullwidth">
+      <thead>
+        <tr>
+          <th>UUID</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in user_projects" :key="project._id">
+          <td><a :href="'/dataset/' + project._id">{{ project._id }}</a></td>
+          <td>{{ project.title }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </div>
 </template>
@@ -61,17 +108,20 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(['user']),
+    ...mapGetters(['user',
+                   'user_orders',
+                   'user_datasets',
+                   'user_projects']),
   },
   created () {
-    this.$store.dispatch('getUser', this.id);
+    this.$store.dispatch('getUser');
+    this.$store.dispatch('getUserOrders');
+    this.$store.dispatch('getUserDatasets');
+    this.$store.dispatch('getUserProjects');
   }
 }
 </script>
 
 <style scoped>
-.data-header {
-    text-align: right;
-    font-weight: bold;
-}
+  
 </style>
--- frontend/src/store/modules/user/index.js
+++ frontend/src/store/modules/user/index.js
@@ -2,11 +2,23 @@ import axios from 'axios';
 
 const state = {
   user: {},
+  user_datasets: [],
+  user_orders: [],
+  user_projects: [],
 }
 
 const mutations = {
   UPDATE_USER (state, payload) {
     state.user = payload;
+  },
+  UPDATE_USER_DATASETS (state, payload) {
+    state.user_datasets = payload;
+  },
+  UPDATE_USER_ORDERS (state, payload) {
+    state.user_orders = payload;
+  },
+  UPDATE_USER_PROJECTS (state, payload) {
+    state.user_projects = payload;
   },
 }
 
@@ -24,10 +36,55 @@ const actions = {
         });
     });
   },
+
+  getUserOrders ({ commit }) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get('/api/order/user')
+        .then((response) => {
+          commit('UPDATE_USER_ORDERS', response.data.orders);
+          resolve(response);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+
+  getUserDatasets ({ commit }) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get('/api/dataset/user')
+        .then((response) => {
+          commit('UPDATE_USER_DATASETS', response.data.datasets);
+          resolve(response);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+
+  getUserProjects ({ commit }) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get('/api/project/user')
+        .then((response) => {
+          commit('UPDATE_USER_PROJECTS', response.data.projects);
+          resolve(response);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
 }
 
 const getters = {
   user: state => state.user,
+  user_orders: state => state.user_orders,
+  user_datasets: state => state.user_datasets,
+  user_projects: state => state.user_projects,
 }
 
 const userModule = {
