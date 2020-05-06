be2e93e78a4e65ae0447d00cc78ca46189b82891
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-05-06 18:10:05 +0200

    Display order data correctly

--- frontend/src/components/orders/OrderAbout.vue
+++ frontend/src/components/orders/OrderAbout.vue
@@ -1,34 +1,34 @@
 <template>
-<div class="dataset-info">
-  <div class="warning" v-if="Object.keys(dataset).length === 0">
-    <span>Unable to retrieve dataset</span>
-  </div>
-  <div v-else>
-    <h1 class="title is-2">{{ dataset.title }}</h1>
-    <div class="dataset-description field" v-html="dataset.description"></div>
-    <table class="table is-hoverable">
-      <tbody>
-        <tr v-if="dataset.creator"><td class="data-header">Creator</td><td>{{ dataset.creator }}</td></tr>
-        <tr v-if="dataset.doi"><td class="data-header">DOI</td><td>{{ dataset.doi }}</td></tr>
-        <tr v-if="dataset.dmp"><td class="data-header"><abbr title="Data management plan">DMP</abbr></td><td><a :href="dataset.dmp">{{ dataset.dmp }}</a></td></tr>
-        <tr v-if="dataset.dataUrls.length > 0">
-        <tr class="test" v-for="(location, index) in dataset.dataUrls" :key="location.id">
-          <td class="data-header"><span v-if="index === 0">Data locations</span></td>
-          <td class="test"><a :href="location.url">{{location.description}}</a></td>
-        </tr>
-        <tr class="test" v-for="(publication, index) in dataset.publications" :key="publication.id">
-          <td class="data-header"><span v-if="index === 0">Publications</span></td>
-          <td class="test"><a :href="publication.url">{{publication}}</a></td>
-        </tr>
-        <tr class="test" v-for="(project, index) in dataset.projects" :key="project.id">
-          <td class="data-header"><span v-if="index === 0">Projects</span></td>
-          <td class="test"><router-link :to="'/project/' + project.uuid + '/about'">{{project.title}}</router-link></td>
-        </tr>
-      </tbody>
-    </table>
-    <span class="timestamp">Last update: {{ dataset.timestamp }}</span>
-  </div>
-  <button class="button is-link" @click="editDataset">
+<div class="order-info">
+  <h1 class="title is-2">{{ order.title }}</h1>
+  <div class="order-description field" v-html="order.description"></div>
+  <table class="table is-hoverable">
+    <tbody>
+      <tr>
+        <th scope="row">Creator</th>
+        <td>{{order.creator}}</td>
+      </tr>
+      <tr>
+        <th scope="row">Receiver</th>
+        <td>{{order.receiver}}</td>
+      </tr>
+      <tr v-if="order.datasets.length > 0">
+        <th scope="row" :rowspan="order.datasets.length">Datasets</th>
+        <td>
+          <ul>
+            <li v-for="dataset in order.datasets" :key="dataset._id">
+              <router-link :to="'/dataset/' + dataset._id">{{dataset.title}}</router-link>
+            </li>
+          </ul>
+        </td>
+      </tr>
+      <tr v-for="field in Object.keys(order.extra)" :key="field">
+        <th scope="row">{{ field }}</th>
+        <td>{{ order.extra[field] }}</td>
+      </tr>
+    </tbody>
+  </table>
+  <button class="button is-link" @click="editOrder">
     Edit
   </button>
 </div>
@@ -38,31 +38,31 @@
 import {mapGetters} from 'vuex';
 
 export default {
-  name: 'DatasetAbout',
-  props: ['id'],
+  name: 'OrderAbout',
+  props: ['uuid'],
   components: {
   },
   computed: {
-    ...mapGetters(['dataset']),
+    ...mapGetters(['order']),
   },
   data () {
     return {
     }
   },
   created () {
-    this.$store.dispatch('getDataset', this.id);
+    this.$store.dispatch('getOrder', this.uuid);
   },
   methods: {
-    editDataset(event) {
+    editOrder(event) {
       event.preventDefault();
-      this.$router.push("/dataset/" + this.$props.id + "/edit");
+      this.$router.push("/order/" + this.$props.uuid + "/edit");
     },
   },
 }
 </script>
 
 <style scoped>
-.dataset-title {
+.order-title {
     font-weight: bold;
     font-size: 2em;
     text-align: center;
