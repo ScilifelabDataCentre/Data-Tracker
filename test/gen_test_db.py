cf54fabaa0160fb3a531f6b882a79ae2efcdea55
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-05-13 13:41:06 +0200

    Fix how extra fields are added to test data

--- test/gen_test_db.py
+++ test/gen_test_db.py
@@ -61,7 +61,7 @@ def gen_datasets(db, nr_datasets: int = 500):
         # add extra field
         if random.random() > 0.7:
             tag = random.choice(EXTRA_KEYS)
-            changes['extra'] = [{tag: random.choice(EXTRA_FIELDS[tag])}]
+            changes['extra'] = {tag: random.choice(EXTRA_FIELDS[tag])}
         dataset.update(changes)
         uuids.append(db['datasets'].insert_one(dataset).inserted_id)
         make_log(db, action='add', data=dataset, data_type='dataset',comment='Generated', user='root')
