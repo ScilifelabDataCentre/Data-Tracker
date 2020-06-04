4d8665c705543737abfd45ee2d5de8d6422148db
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-06-04 15:09:18 +0200

    Updated style of user manager

--- frontend/src/views/admin/UserManager.vue
+++ frontend/src/views/admin/UserManager.vue
@@ -1,26 +1,27 @@
 <template>
 <div class="user-manager">
-  <div v-else>
-    <h1 id="user-table-header" class="title is-1">List of users</h1>
-    <table class="table is-hoverable is-striped" v-if="users.length > 0" aria-describedby="user-table-header">
-      <thead>
-        <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Authentication ID</th>
-          <th scope="col">Email</th>
-          <th scope="col">Affiliation</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v @click="editUser($event, user._id)" v-for="user in users" :key="user._id">
-          <td>{{user.name}}</td>
-          <td>{{user.authId}}</td>
-          <td>{{user.email}}</td>
-          <td>{{user.affiliation}}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <h1 id="user-table-header" class="title is-1">List of users</h1>
+  <router-link to="user/add">
+    <img class="icon-add" :src="require('../../assets/open-iconic/svg/plus.svg')" alt="Add" />
+  </router-link>
+  <table class="table is-hoverable is-striped" v-if="users.length > 0" aria-describedby="user-table-header">
+    <thead>
+      <tr>
+        <th scope="col">Name</th>
+        <th scope="col">Authentication ID</th>
+        <th scope="col">Email</th>
+        <th scope="col">Affiliation</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v @click="editUser($event, user._id)" v-for="user in users" :key="user._id">
+        <td>{{user.name}}</td>
+        <td>{{user.authId}}</td>
+        <td>{{user.email}}</td>
+        <td>{{user.affiliation}}</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 </template>
 
