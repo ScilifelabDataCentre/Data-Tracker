fceecfd55e32234136836cc645759563ea5603ec
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-06-11 13:57:44 +0200

    5 fields in response now

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -46,7 +46,7 @@ def test_list_info():
                                        ret_json=True)
     for response in responses:
         assert response.code == 200
-        assert len(response.data['user']) == 4
+        assert len(response.data['user']) == 5
         if response.role != 'no-login':
             assert response.data['user']['name'] == f'{response.role.capitalize()} Test'
 
