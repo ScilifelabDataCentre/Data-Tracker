c1e15346f71549e692c612fba5bfd16a3d6c8d7d
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-06-12 13:47:57 +0200

    Add sections, fix headers etc

--- frontend/src/views/user/CurrentUserAbout.vue
+++ frontend/src/views/user/CurrentUserAbout.vue
@@ -1,106 +1,117 @@
 <template>
 <div class="user-about">
-  <h4 class="title is-4">User Information</h4>
-  <table class="table is-hoverable is-striped">
-    <tbody>
-      <tr>
-        <th scope="row">Name</th>
-        <td>{{ user.name }}</td>
-      </tr>
-      <tr>
-        <th scope="row">Affiliation</th>
-        <td>{{ user.affiliation }}</td>
-      </tr>
-      <tr>
-        <th scope="row">Email</th>
-        <td>{{ user.email }}</td>
-      </tr>
-      <tr>
-        <th scope="row">Authentication ID</th>
-        <td>{{ user.authId }}</td>
-      </tr>
-      <tr v-if="user.permissions.length > 0">
-        <th scope="row" :rowspan="user.permissions.length">Permissions</th>
-        <td>
-          <ul>
-            <li v-for="entry in user.permissions" :key="entry">
-              <span class="tag is-info is-light">
-                {{entry}}
-              </span>
-            </li>
-          </ul>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <h1 class="title is-1">About the Current User ({{ user.name }})</h1>
+
+  <section class="section">
+    <h2 class="title is-3">Basic Information</h2>
+    <table class="table is-hoverable is-striped">
+      <tbody>
+	<tr>
+          <th scope="row">Name</th>
+          <td>{{ user.name }}</td>
+	</tr>
+	<tr>
+          <th scope="row">Affiliation</th>
+          <td>{{ user.affiliation }}</td>
+	</tr>
+	<tr>
+          <th scope="row">Email</th>
+          <td>{{ user.email }}</td>
+	</tr>
+	<tr>
+          <th scope="row">Authentication ID</th>
+          <td>{{ user.authId }}</td>
+	</tr>
+	<tr v-if="user.permissions.length > 0">
+          <th scope="row" :rowspan="user.permissions.length">Permissions</th>
+          <td>
+            <ul>
+              <li v-for="entry in user.permissions" :key="entry">
+		<span class="tag is-info is-light">
+                  {{entry}}
+		</span>
+              </li>
+            </ul>
+          </td>
+	</tr>
+      </tbody>
+    </table>
 
   <router-link to="edit">
     <button class="button is-link">
       Edit
     </button>
   </router-link>
-  
-  <div v-if="userOrders.length > 0">
-    <h4 class="title is-4">Orders</h4>
-    <table class="table is-hoverable is-striped">
-      <thead>
-        <tr>
-          <th>UUID</th>
-          <th>Title</th>
-          <th>Datasets</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="order in userOrders" :key="order._id">
-          <td><a :href="'/dataset/' + order._id">{{ order._id }}</a></td>
-          <td>{{ order.title }}</td>
-          <td>
-            <ul>
-              <li v-for="ds_uuid in order.datasets" :key="ds_uuid">
-                <a :href="'/dataset/' + ds_uuid">{{ ds_uuid }}</a>
-              </li>
-            </ul>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  </section>
 
-  <div v-if="userDatasets.length > 0">
-    <h4 class="title is-4">Datasets</h4>
-    <table class="table is-hoverable is-striped">
-      <thead>
-        <tr>
-          <th>UUID</th>
-          <th>Title</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="dataset in userDatasets" :key="dataset._id">
-          <td><a :href="'/dataset/' + dataset._id">{{ dataset._id }}</a></td>
-          <td>{{ dataset.title }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <section class="section">
+    <div v-if="userOrders.length > 0">
+      <h2 class="title is-3">Orders</h2>
+      <table class="table is-hoverable is-striped">
+	<thead>
+          <tr>
+            <th>UUID</th>
+            <th>Title</th>
+            <th>Datasets</th>
+          </tr>
+	</thead>
+	<tbody>
+          <tr v-for="order in userOrders" :key="order._id">
+            <td><a :href="'/dataset/' + order._id">{{ order._id }}</a></td>
+            <td>{{ order.title }}</td>
+            <td>
+              <ul>
+		<li v-for="ds_uuid in order.datasets" :key="ds_uuid">
+                  <a :href="'/dataset/' + ds_uuid">{{ ds_uuid }}</a>
+		</li>
+              </ul>
+            </td>
+          </tr>
+	</tbody>
+      </table>
+    </div>
+  </section>
 
-  <div v-if="userProjects.length > 0">
-    <h4 class="title is-4">Projects</h4>
-    <table class="table is-hoverable is-striped is-fullwidth">
-      <thead>
-        <tr>
-          <th>UUID</th>
-          <th>Title</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="project in userProjects" :key="project._id">
-          <td><a :href="'/dataset/' + project._id">{{ project._id }}</a></td>
-          <td>{{ project.title }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <section class="section">
+    <div v-if="userDatasets.length > 0">
+      <h2 class="title is-3">Datasets</h2>
+      <table class="table is-hoverable is-striped">
+	<thead>
+          <tr>
+            <th>UUID</th>
+            <th>Title</th>
+          </tr>
+	</thead>
+	<tbody>
+          <tr v-for="dataset in userDatasets" :key="dataset._id">
+            <td><a :href="'/dataset/' + dataset._id">{{ dataset._id }}</a></td>
+            <td>{{ dataset.title }}</td>
+          </tr>
+	</tbody>
+      </table>
+    </div>
+  </section>
+
+
+  <section class="section">
+    <div v-if="userProjects.length > 0">
+      <h2 class="title is-3">Projects</h2>
+      <table class="table is-hoverable is-striped is-fullwidth">
+	<thead>
+          <tr>
+            <th>UUID</th>
+            <th>Title</th>
+          </tr>
+	</thead>
+	<tbody>
+          <tr v-for="project in userProjects" :key="project._id">
+            <td><a :href="'/dataset/' + project._id">{{ project._id }}</a></td>
+            <td>{{ project.title }}</td>
+          </tr>
+	</tbody>
+      </table>
+    </div>
+  </section>
 </div>
 </template>
 
