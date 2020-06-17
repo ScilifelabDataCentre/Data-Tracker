91615113ae810e86598c0000429a1908fe0b0e24
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-06-17 13:20:03 +0200

    Remove unused button

--- frontend/src/views/datasets/DatasetBrowser.vue
+++ frontend/src/views/datasets/DatasetBrowser.vue
@@ -1,9 +1,6 @@
 <template>
 <div class="dataset-browser">
   <h1 class="title is-2">Datasets</h1>
-  <router-link v-if="user.role === 'Steward' || user.role === 'Admin'" to="/dataset/add">
-    <img class="icon-add" :src="require('../../assets/open-iconic/svg/plus.svg')" alt="Add" />
-  </router-link>
   <browser-entry v-for="dataset in datasets" :key="dataset.id" :entry="dataset" entry_type="dataset">
   </browser-entry>
 </div>
