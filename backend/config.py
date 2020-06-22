92bc2e09a50bb9996a8854f704f17cad30d58419
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-06-22 15:46:13 +0200

    Delete oidc after loading in config

--- backend/config.py
+++ backend/config.py
@@ -68,6 +68,7 @@ def init() -> dict:
             base_name = oidc_entry.upper()
             for conf_part in config['oidc'][oidc_entry]:
                 config[f'{base_name}_{conf_part.upper()}'] = config['oidc'][oidc_entry][conf_part]
+        del config['oidc']
 
     config['SESSION_COOKIE_NAME'] = 'dt_session'
     config['SECRET_KEY'] = config['flask']['secret']
