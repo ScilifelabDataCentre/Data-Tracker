2951f0f03651e14c6ec98edd893e1ed9024770ea
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-07-04 19:37:51 +0200

    Skeletons for displaying datasets

---
+++ quasar/src/pages/datasets/DatasetBrowser.vue
@@ -0,0 +1,58 @@
+<template>
+<q-page padding>
+  <q-table
+    title="Datasets"
+    :data="datasets"
+    :columns="columns"
+    row-key="id"
+    :pagination.sync="pagination"
+    /></q-page>
+</template>
+
+<script>
+export default {
+  name: 'DatasetBrowser',
+  
+  computed: {
+    datasets: {
+      get () {
+        return this.$store.state.datasets.datasets;
+      },
+    }
+  },
+
+  data () {
+    return {
+      pagination: {
+        rowsPerPage: 20
+      },
+
+      columns: [
+        {
+          name: 'id',
+          label: 'Identifier (UUID)',
+          field: '_id',
+          required: true,
+          align: 'left',
+          sortable: true,
+
+          // style: 'width: 500px',
+          // classes: 'my-special-class',
+          // headerStyle: 'width: 500px',
+          // headerClasses: 'my-special-class'
+        },
+        { name: 'title',
+          label: 'Dataset title',
+          field: 'title',
+          required: true,
+          sortable: true
+        }
+      ]
+    }
+  },
+
+  mounted () {
+    this.$store.dispatch('datasets/getDatasets');
+  }
+}
+</script>
