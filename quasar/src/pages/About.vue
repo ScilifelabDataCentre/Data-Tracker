736f96a08e87e8963dced07eb687fc1efa3e13e8
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-07-04 18:29:50 +0200

    Updated about skeleton

--- quasar/src/pages/About.vue
+++ quasar/src/pages/About.vue
@@ -5,12 +5,25 @@
 
     <p>Some more text.</p>
 
+    <p>Track data. Only metadata.</p>
+
+    <h2 class="text-h3">Background</h2>
+
+    <h2 class="text-h3">Data types</h2>
+    
+    <h3 class="text-h4">Orders</h3>
+
+    <h3 class="text-h4">Datasets</h3>
+
+    <h3 class="text-h4">Collections</h3>
+
+    <h2 class="text-h3">Contact</h2>
     
   </q-page>
 </template>
 
 <script>
 export default {
-  // name: 'PageName',
+  name: 'About',
 }
 </script>
