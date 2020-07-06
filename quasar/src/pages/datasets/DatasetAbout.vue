5ade611523d15590265053708c96ea1594a0e227
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-07-06 22:47:03 +0200

    Add e.g. edit button and support for markdown

---
+++ quasar/src/pages/datasets/DatasetAbout.vue
@@ -0,0 +1,52 @@
+<template>
+<q-page padding>
+  <q-inner-loading :showing="loading">
+    <q-spinner-gears size="100px" color="primary" />
+  </q-inner-loading>
+  <q-page-sticky position="bottom-right" :offset="[24, 24]">
+    <q-btn fab icon="edit" color="accent" />
+  </q-page-sticky>
+  <q-card>
+    <q-card-section>
+      <div class="text-center">
+        <h1 class="text-h2">{{ dataset.title }}</h1>
+        <div class="text-italic">
+          {{ dataset._id }}
+        </div>
+      </div>
+    </q-card-section>
+    <q-card-section>
+      <q-markdown :src="dataset.description" />
+    </q-card-section>
+  </q-card>
+</q-page>
+</template>
+
+<script>
+export default {
+  name: 'DatasetAbout',
+  props: ['uuid'],
+
+  computed: {
+    dataset: {
+      get () {
+        return this.$store.state.datasets.dataset;
+      },
+    },
+  },
+
+  data () {
+    return {
+      loading: true,
+    }
+  },
+  
+  mounted () {
+    this.$store.dispatch('datasets/getDataset', this.uuid)
+      .then(() => this.loading = false)
+      .catch(() => this.loading = false)
+
+  }
+  
+}
+</script>
