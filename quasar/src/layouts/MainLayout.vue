26575ffe14f175c65ebd798182d67028529ed379
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-07-09 07:16:16 +0200

    Add breadcrumbs

--- quasar/src/layouts/MainLayout.vue
+++ quasar/src/layouts/MainLayout.vue
@@ -10,14 +10,13 @@
           aria-label="Menu"
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
-
         <q-toolbar-title>
           <q-btn
             flat
             dense
             no-caps
             no-wrap
-            to="/"
+            :to="{'name': 'Home'}"
             label="Data Tracker"
             icon="home" />
         </q-toolbar-title>
@@ -32,6 +31,12 @@
 	  target="_blank"
 	  href="https://www.scilifelab.se/data/"
           />
+      </q-toolbar>
+      <q-toolbar inset>
+        <q-breadcrumbs active-color="white" style="font-size: 16px">
+          <q-breadcrumbs-el label="Home" icon="home" to="/"/>
+          <q-breadcrumbs-el v-for="(part, i) in breadcrumbs" :key="part" :label="part" :to="'/'+breadcrumbs.slice(0, i-1).join('/')"/>
+        </q-breadcrumbs>
       </q-toolbar>
     </q-header>
 
@@ -62,12 +67,25 @@ export default {
     MainDrawerContent
   },
 
+  watch: {
+    '$route' () {
+      let splitPath = this.$route.path.split('/').slice(1);
+      if (splitPath[0]) this.breadcrumbs = splitPath; else this.breadcrumbs = [];
+    }
+  },
+  
   data () {
     return {
       leftDrawerOpen: false,
+      breadcrumbs: [],
     }
   },
 
+  mounted () {
+    let splitPath = this.$route.path.split('/').slice(1);
+    if (splitPath[0]) this.breadcrumbs = splitPath; else this.breadcrumbs = [];
+  },
+  
   created () {
     this.$store.dispatch('currentUser/getInfo');
   },
