974eb4e7ab6e1034247b6b357da3625dec840612
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-07-09 07:07:03 +0200

    Update routes; regex for uuid etc

--- quasar/src/router/routes.js
+++ quasar/src/router/routes.js
@@ -1,24 +1,34 @@
 import MainLayout from 'layouts/MainLayout.vue'
 import Index from 'pages/Index.vue'
 import About from 'pages/About.vue'
-import DatasetBase from 'pages/datasets/DatasetBase.vue'
+import DatasetAbout from 'pages/datasets/DatasetAbout.vue'
 import DatasetBrowser from 'pages/datasets/DatasetBrowser.vue'
+
 
 const routes = [
   {
     path: '/',
     component:  MainLayout,
     children: [
-      { path: '', component: Index },
-      { path: 'about', component: About },
+      { path: '', component: Index, name: 'Home'},
+      { path: 'about', component: About, name: 'About' },
     ]
   },
   {
     path: '/datasets',
     component:  MainLayout,
     children: [
-      { path: '', redirect: 'browser' },
-      { path: 'browser', component: DatasetBrowser },
+      {
+        path: '',
+        component: DatasetBrowser,
+        name: 'Dataset Browser'
+      },
+      {
+        path: ':uuid([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})',
+        component: DatasetAbout,
+        props: true,
+        name: 'Dataset About'
+      },
     ]
   },
 
