8ec9b611e3e140466ce92b918483f4780403b19f
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-07-09 22:15:34 +0200

    Add order pages, split bundles

--- quasar/src/router/routes.js
+++ quasar/src/router/routes.js
@@ -1,9 +1,15 @@
 import MainLayout from 'layouts/MainLayout.vue'
-import Index from 'pages/Index.vue'
-import About from 'pages/About.vue'
-import DatasetAbout from 'pages/datasets/DatasetAbout.vue'
-import DatasetBrowser from 'pages/datasets/DatasetBrowser.vue'
-import DatasetEdit from 'pages/datasets/DatasetEdit.vue'
+
+const Index = () => import(/* webpackChunkName: "base" */ 'pages/Index.vue')
+const About = () => import(/* webpackChunkName: "base" */ 'pages/About.vue')
+
+const DatasetAbout = () => import(/* webpackChunkName: "dataset" */ 'pages/datasets/DatasetAbout.vue')
+const DatasetBrowser = () => import(/* webpackChunkName: "dataset" */ 'pages/datasets/DatasetBrowser.vue')
+const DatasetEdit = () => import(/* webpackChunkName: "dataset" */ 'pages/datasets/DatasetEdit.vue')
+
+const OrderAbout = () => import(/* webpackChunkName: "order" */ 'pages/orders/OrderAbout.vue')
+const OrderBrowser = () => import(/* webpackChunkName: "order" */ 'pages/orders/OrderBrowser.vue')
+const OrderEdit = () => import(/* webpackChunkName: "order" */ 'pages/orders/OrderEdit.vue')
 
 const routes = [
   {
@@ -14,6 +20,7 @@ const routes = [
       { path: 'about', component: About, name: 'About' },
     ]
   },
+
   {
     path: '/datasets',
     component:  MainLayout,
@@ -35,11 +42,35 @@ const routes = [
         props: true,
         name: 'Dataset Edit'
       },
+    ]
+  },
+  
+  {
+    path: '/orders',
+    component:  MainLayout,
+    children: [
+      {
+        path: '',
+        component: OrderBrowser,
+        name: 'Order Browser'
+      },
+      {
+        path: ':uuid([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})',
+        component: OrderAbout,
+        props: true,
+        name: 'Order About'
+      },
+      {
+        path: ':uuid([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/edit',
+        component: OrderEdit,
+        props: true,
+        name: 'Order Edit'
+      },
       {
         path: 'add',
-        component: DatasetEdit,
+        component: OrderEdit,
         props: { 'uuid': '' },
-        name: 'Dataset Add'
+        name: 'Order Add'
       },
     ]
   },
