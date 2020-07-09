f8e720080b257db0563e7f6dd9169dd710fa51a4
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-07-09 08:13:57 +0200

    Use old display style for other dataset fields

--- quasar/src/pages/datasets/DatasetAbout.vue
+++ quasar/src/pages/datasets/DatasetAbout.vue
@@ -3,7 +3,10 @@
   <q-inner-loading :showing="loading">
     <q-spinner-gears size="100px" color="primary" />
   </q-inner-loading>
-  <q-page-sticky position="bottom-right" :offset="[24, 24]">
+  <q-page-sticky position="bottom-right"
+                 :offset="[24, 24]"
+                 v-if="dataset.creator === currentUser.name || 
+                       currentUser.permissions.includes('DATA_MANAGEMENT')">
     <q-btn fab icon="edit" color="accent" />
   </q-page-sticky>
       <div class="text-center q-mb-lg">
@@ -19,7 +22,58 @@
   </q-card>
   <q-card class="q-my-lg">
     <q-card-section>
-      <q-markdown :src="dataset.extra" />
+      <table>
+        <tbody>
+          <tr v-if="dataset.creator.length > 0">
+            <th scope="row">
+              Creator
+            </th>
+            <td>
+              {{ dataset.creator }}
+            </td>
+          </tr>
+          <tr v-if="dataset.links.length > 0">
+            <th scope="row">
+              Links
+            </th>
+            <td>
+              <q-list>
+                <q-item v-for="location in dataset.links" :key="location.description">
+                  <q-btn flat no-caps :label="location.description" type="a" :href="location.url" />
+                </q-item>
+              </q-list>
+            </td>
+          </tr>
+          <tr v-for="field in Object.keys(dataset.extra)" :key="field">
+            <th class="capitalize" scope="row">{{ field }}</th>
+            <td>{{ dataset.extra[field] }}</td>
+          </tr>
+          <tr v-if="dataset.projects.length > 0">
+            <th scope="row">
+              Projects
+            </th>
+            <td>
+              <q-list>
+                <q-item v-for="project in dataset.projects" :key="project._id">
+                  <q-btn flat no-caps :label="project.title" :to="'/project/' + project._id" />
+                </q-item>
+              </q-list>
+            </td>
+          </tr>
+          <tr v-if="dataset.related.length > 0">
+            <th scope="row">
+              Related datasets
+            </th>
+            <td>
+              <q-list>
+                <q-item v-for="dataset in dataset.related" :key="dataset._id">
+                  <q-btn flat no-caps :label="dataset.title" :to="'/dataset/' + dataset._id"></q-btn>
+                </q-item>
+              </q-list>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </q-card-section>
   </q-card>
 </q-page>
@@ -36,6 +90,11 @@ export default {
         return this.$store.state.datasets.dataset;
       },
     },
+    currentUser: {
+      get () {
+        return this.$store.state.currentUser.info;
+      },
+    },
   },
 
   data () {
@@ -48,7 +107,6 @@ export default {
     this.$store.dispatch('datasets/getDataset', this.uuid)
       .then(() => this.loading = false)
       .catch(() => this.loading = false)
-
   }
   
 }
