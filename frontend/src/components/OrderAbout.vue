d39d0c41b5524a77b6de9952d9dee06f87c80beb
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-09-23 14:01:39 +0200

    Various updates to avoid errors

--- frontend/src/components/OrderAbout.vue
+++ frontend/src/components/OrderAbout.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<div v-if="Object.keys(order).length > 0">
   <div class="text-center q-mb-lg">
     <h1 class="text-h2 q-mb-xs ">{{ order.title }}</h1>
     <div class="text-subtitle1 text-italic">
@@ -16,59 +16,61 @@
   <q-card>
     <q-card-section>
       <q-list dense>
-        <list-header title="Authors"
-                    explanation="The ones who provided the sample, e.g. a researcher" />
-        <user-entry v-for="author in order.authors"
-                    :key="author._id"
-                    v-bind="author" />
+        <div v-show="order.authors.length > 0">
+          <list-header title="Authors"
+                       explanation="The ones who provided the sample, e.g. a researcher" />
+          <user-entry v-for="author in order.authors"
+                      :key="author._id"
+                      v-bind="author" />
+        </div>
 
-        <q-item-label header>
-          <span>
-            Generators
-            <q-tooltip>
-              The ones who generated the data, e.g. a facility
-            </q-tooltip>
-          </span>
-        </q-item-label>
-        <user-entry v-for="generator in order.generators"
-                    :key="generator._id"
-                    v-bind="generator" />
+        <div v-show="order.generators.length > 0">
+          <list-header title="Generators"
+                       explanation="The ones who generated the data, e.g. a facility" />
+          <user-entry v-for="generator in order.generators"
+                      :key="generator._id"
+                      v-bind="generator" />
+        </div>
 
-        <list-header title="Organisation"
-                    explanation="The data owner, e.g. a university" />
-        <user-entry v-bind="order.organisation" />
+        <div v-show="order.organisation.length > 0">
+          <list-header title="Organisation"
+                       explanation="The data owner, e.g. a university" />
+          <user-entry v-bind="order.organisation" />
+        </div>
 
-        <list-header title="Editors"
-                    explanation="Users that may edit this order" />
-        <user-entry v-for="entry in order.editors"
-                    :key="entry._id"
-                    v-bind="entry" />
+        <div v-show="order.editors.length > 0">
+          <list-header title="Editors"
+                       explanation="Users that may edit this order" />
+          <user-entry v-for="entry in order.editors"
+                      :key="entry._id"
+                      v-bind="entry" />
+        </div>
       </q-list>
     </q-card-section>
-  </q-card>
-  <q-card class="q-my-md">
     <q-card-section>
-      <q-list dense>
-        <list-header title="Datasets"
-                     explanation="Datasets generated from this order" />
-        <q-item v-for="dataset in order.datasets" :key="dataset._id">
-          <q-item-section avatar>
-            <q-btn flat
-                   dense
-                   round
-                   icon="link"
-                   :to="{ 'name': 'Dataset About', 'params': { 'uuid': dataset._id } }" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>
-              {{ dataset.title }}
-            </q-item-label>
-            <q-item-label caption>
-              {{ dataset._id }}
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
+      <div v-show="order.datasets.length > 0">
+        <q-list dense>
+          <list-header title="Datasets"
+                       explanation="Datasets generated from this order" />
+          <q-item v-for="dataset in order.datasets" :key="dataset._id">
+            <q-item-section avatar>
+              <q-btn flat
+                     dense
+                     round
+                     icon="link"
+                     :to="{ 'name': 'Dataset About', 'params': { 'uuid': dataset._id } }" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>
+                {{ dataset.title }}
+              </q-item-label>
+              <q-item-label caption>
+                {{ dataset._id }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
       
       <q-field
         v-for="field in Object.keys(order.tagsUser)" :key="field"
@@ -110,7 +112,6 @@ export default {
 
   data () {
     return {
-      loading: true,
     }
   },
 
