4de5a70e87cbe340835ac90d37b0185bcefa391a
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-09-23 14:52:12 +0200

    Make UserInfo a q-expansion-item

--- frontend/src/components/UserEntry.vue
+++ frontend/src/components/UserEntry.vue
@@ -1,65 +1,59 @@
 <template>
-<q-item class=".col-12"
-        clickable
-        @click="isOpen = !isOpen" >
-  <q-item-section>
-    {{ name }}
-    <q-slide-transition>
-      <div v-show="isOpen">
-        <q-list dense>
-          <q-item v-show="contact.length > 0">
-            <q-item-section avatar>
-              <q-avatar>
-                <q-icon name="contact_mail" />
-              </q-avatar>
-            </q-item-section>
-            <q-item-section>
-              {{ contact }}
-            </q-item-section>
-          </q-item>
-          <q-item v-show="affiliation.length > 0">
-            <q-item-section avatar>
-              <q-avatar>
-                <q-icon name="business" />
-              </q-avatar>
-            </q-item-section>
-            <q-item-section>
-              {{ affiliation }}
-            </q-item-section>
-          </q-item>
-          <q-item v-show="url.length > 0">
-            <q-item-section avatar>
-              <q-avatar>
-                <q-icon name="link" />
-              </q-avatar>
-            </q-item-section>
-            <q-item-section>
-              {{ url }}
-            </q-item-section>
-          </q-item>
-          <q-item v-show="orcid.length > 0">
-            <q-item-section avatar>
-              <q-avatar>
-                <q-icon name="account_circle" />
-              </q-avatar>
-            </q-item-section>
-            <q-item-section>
-              {{ orcid }}
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </div>
-    </q-slide-transition>
-  </q-item-section>
-  <q-item-section avatar>
-    <q-icon :name="isOpen ? 'arrow_drop_up' : 'arrow_drop_down'" />
-  </q-item-section>
-</q-item>
+<q-expansion-item expand-separator
+                  icon="far fa-user"
+                  :label="name">
+  <q-card>
+    <q-card-section>
+      <q-list dense>
+        <q-item v-show="contact.length > 0">
+          <q-item-section avatar>
+            <q-avatar>
+              <q-icon name="far fa-address-card"/>
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            {{ contact }}
+          </q-item-section>
+        </q-item>
+        <q-item v-show="affiliation.length > 0">
+          <q-item-section avatar>
+            <q-avatar>
+              <q-icon name="fas fa-university" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            {{ affiliation }}
+          </q-item-section>
+        </q-item>
+        <q-item v-show="url.length > 0">
+          <q-item-section avatar>
+            <q-avatar>
+              <q-icon name="fas fa-link" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <a class="std-link" :href="url">{{ url }}</a>
+          </q-item-section>
+        </q-item>
+        <q-item v-show="orcid.length > 0">
+          <q-item-section avatar>
+            <q-avatar>
+              <q-icon name="fab fa-orcid" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <a class="std-link" :href="'https://orcid.org/' + orcid">{{ orcid }}</a>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-card-section>
+  </q-card>
+</q-expansion-item>
 </template>
 
 <script>
 export default {
-  name: 'UserInfo',
+  name: 'UserEntry',
 
   props: {
     name: {
