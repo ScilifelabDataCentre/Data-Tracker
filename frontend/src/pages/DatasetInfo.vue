8630231fe78084aec9816e13207eaf6f61f44f24
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-10-08 13:32:04 +0200

    Split Dataset page

---
+++ frontend/src/pages/DatasetInfo.vue
@@ -0,0 +1,49 @@
+<template>
+<q-page padding>
+  <dataset-about />
+</q-page>
+</template>
+
+<script>
+import DatasetAbout from 'components/DatasetAbout.vue'
+
+export default {
+  name: 'DatasetInfo',
+
+ components: {'dataset-about': UserInfo},
+  
+  props: {
+    uuid: {
+      type: String,
+      required: true
+    }
+  },
+
+  computed: {
+    dataset: {
+      get () {
+        return this.$store.state.datasets.dataset;
+      },
+    },
+    currentUser: {
+      get () {
+        return this.$store.state.currentUser.info;
+      },
+    },
+  },
+
+  data () {
+    return {
+      loading: true,
+    }
+  },
+  
+  mounted () {
+    this.$store.dispatch('datasets/getDataset', this.uuid)
+      .then(() => this.loading = false)
+      .catch(() => this.loading = false)
+  }
+  
+}
+</script>
+
