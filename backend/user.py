f3db3d73a8fbfc26233ec9039666ba0c7c6c0359
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-10-26 16:20:13 +0100

    Make it possible to actually save data when email is unchanged

--- backend/user.py
+++ backend/user.py
@@ -343,7 +343,7 @@ def update_user_info(identifier: str):
 
     if 'email' in indata:
         old_user = flask.g.db['users'].find_one({'email': indata['email']})
-        if old_user:
+        if old_user and old_user['_id'] != user_data['_id']:
             flask.current_app.logger.debug('User already exists')
             flask.abort(status=400)
 
