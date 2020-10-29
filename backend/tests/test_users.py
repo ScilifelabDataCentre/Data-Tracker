1e4a7050768a13c4a5b382a8b35f65604517ead2
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-10-29 14:44:38 +0100

    Update test with correct number of fields

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -33,7 +33,7 @@ def test_list_info():
                                        ret_json=True)
     for response in responses:
         assert response.code == 200
-        assert len(response.data['user']) == 8
+        assert len(response.data['user']) == 9
         if response.role != 'no-login':
             assert response.data['user']['name'] == f'{response.role.capitalize()}'
 
