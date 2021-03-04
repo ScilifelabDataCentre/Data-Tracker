b330fd0062a8ffd1cc894ae7ee1e7662bb8e2710
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-03-04 13:37:34 +0100

    Clean up debug

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -19,29 +19,21 @@ export default function (store /*, ssrContext } */) {
   Router.beforeEach((to, from, next) => {
     if (to.matched.some(record => record.meta.loginRequired)) {
       if (!store.store.getters['currentUser/infoLoaded']) {
-        console.log('data not loaded');
-        console.log(store.store);
         store.store.dispatch('currentUser/getInfo')
           .then(() => {
-            console.log('data loaded');
-            console.log(store.store);
             if (!store.store.getters['currentUser/isLoggedIn']) {
-              console.log('ld, not logged in');
               next({name: 'Login', params: {origin: {name: to.name, path: to.path}}});
             }
             else {
-              console.log('ld, logged in');
               next();
             }
           })
       }
       else {
-        console.log('logged in');
         next();
       }
     }
     else {
-      console.log('default goto next')
       next();
     }
   })
