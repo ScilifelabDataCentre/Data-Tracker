37e7781c14c1400c2f39a79b91f728a64fd3b4a0
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-03-16 12:27:30 +0100

    Remove commented code

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,7 +5,7 @@ import routes from './routes'
 
 Vue.use(VueRouter)
 
-export default function (store /*, ssrContext } */) {
+export default function (store) {
   const Router = new VueRouter({
     scrollBehavior: () => ({ x: 0, y: 0 }),
     routes,
