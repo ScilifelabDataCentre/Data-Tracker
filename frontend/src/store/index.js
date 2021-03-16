b024b18d6a85bbfdc9104b96210f1b69694b098f
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-03-16 12:28:08 +0100

    Remove commented code

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -9,16 +9,7 @@ import entries from './entries'
 
 Vue.use(Vuex)
 
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
-
-export default function (/* { ssrContext } */) {
+export default function () {
   const Store = new Vuex.Store({
     modules: {
       adminUsers,
@@ -28,8 +19,6 @@ export default function (/* { ssrContext } */) {
       entries,
     },
 
-    // enable strict mode (adds overhead!)
-    // for dev mode only
     strict: process.env.DEV
   })
 
