9ff13608f44c9338fa0326c8715c22a2c4431483
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-04-07 13:40:02 +0200

    Test for the auth_ids validation

--- backend/tests/test_validation.py
+++ backend/tests/test_validation.py
@@ -18,3 +18,18 @@ def test_validate_affiliation():
         validator(("asd",))
     with pytest.raises(ValueError):
         validator(4.5)
+
+
+def test_validate_auth_ids():
+    """Confirm that only valid strings are accepted."""
+    validator = validate.VALIDATION_MAPPER["auth_ids"]
+    assert validator(["Test"])
+    assert validator(["Test", "Test 2"])
+    with pytest.raises(ValueError):
+        validator(5)
+    with pytest.raises(ValueError):
+        validator("asd")
+    with pytest.raises(ValueError):
+        validator([1, 2, 3, 4])
+    with pytest.raises(ValueError):
+        validator(4.5)
