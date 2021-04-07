278314fbc2513d20e5f12e0a313aeb4ad8eeecff
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-04-07 11:14:08 +0200

    Formatting change

--- backend/migrations.py
+++ backend/migrations.py
@@ -10,10 +10,16 @@ Version 1 to 2 should run ``MIGRATIONS[1:2]``, i.e. the function at
 ``MIGRATIONS[1]`` should be run.
 """
 
+
 def migrate_v1_to_v2(db):
     """Rename all ORDERS permissions to DATA_EDIT to match the updated permission names."""
-    db["users"].update_many({"permissions": "ORDERS"}, {"$push": {"permissions": "DATA_EDIT"}})
-    db["users"].update_many({"permissions": "ORDERS"}, {"$pull": {"permissions": "ORDERS"}})
+    db["users"].update_many(
+        {"permissions": "ORDERS"}, {"$push": {"permissions": "DATA_EDIT"}}
+    )
+    db["users"].update_many(
+        {"permissions": "ORDERS"}, {"$pull": {"permissions": "ORDERS"}}
+    )
+
 
 # Position 0 is empty since the first release is 1
 MIGRATIONS = [None, migrate_v1_to_v2]
