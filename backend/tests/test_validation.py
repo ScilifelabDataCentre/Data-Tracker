91e2d6584fc4b7510c31e2f46e427766f4bf2eb3
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-04-07 13:58:15 +0200

    Add more validation tests

--- backend/tests/test_validation.py
+++ backend/tests/test_validation.py
@@ -3,6 +3,7 @@ import uuid
 
 import pytest
 
+from helpers import mdb
 import validate
 
 
@@ -21,7 +22,7 @@ def test_validate_affiliation():
 
 
 def test_validate_auth_ids():
-    """Confirm that only valid strings are accepted."""
+    """Confirm that only valid lists of strings are accepted."""
     validator = validate.VALIDATION_MAPPER["auth_ids"]
     assert validator(["Test"])
     assert validator(["Test", "Test 2"])
@@ -33,3 +34,111 @@ def test_validate_auth_ids():
         validator([1, 2, 3, 4])
     with pytest.raises(ValueError):
         validator(4.5)
+
+
+def test_validate_authors(mdb):
+    """Confirm that only valid users are accepted."""
+    validator = validate.VALIDATION_MAPPER["authors"]
+    test_users = [str(entry['_id']) for entry in mdb["users"].aggregate([{"$sample": {"size": 5}}])]
+    assert validator(test_users, db=mdb)
+    assert validator(test_users[:1], db=mdb)
+    with pytest.raises(ValueError):
+        validator(test_users[0], db=mdb)
+    with pytest.raises(ValueError):
+        validator([str(uuid.uuid4()) for _ in range(4)], db=mdb)
+    with pytest.raises(ValueError):
+        validator(5, db=mdb)
+    with pytest.raises(ValueError):
+        validator("asd", db=mdb)
+    with pytest.raises(ValueError):
+        validator([1, 2, 3, 4], db=mdb)
+    with pytest.raises(ValueError):
+        validator(4.5, db=mdb)
+
+
+def test_validate_contact():
+    """Confirm that only valid strings are accepted."""
+    validator = validate.VALIDATION_MAPPER["contact"]
+    assert validator("Test")
+    with pytest.raises(ValueError):
+        validator(5)
+    with pytest.raises(ValueError):
+        validator(["asd"])
+    with pytest.raises(ValueError):
+        validator(("asd",))
+    with pytest.raises(ValueError):
+        validator(4.5)
+
+
+def test_validate_datasets(mdb):
+    """Confirm that only valid users are accepted."""
+    validator = validate.VALIDATION_MAPPER["datasets"]
+    test_datasets = [str(entry['_id']) for entry in mdb["datasets"].aggregate([{"$sample": {"size": 5}}])]
+    assert validator(test_datasets, db=mdb)
+    assert validator(test_datasets[:1], db=mdb)
+    with pytest.raises(ValueError):
+        validator(test_datasets[0], db=mdb)
+    with pytest.raises(ValueError):
+        validator([str(uuid.uuid4()) for _ in range(4)], db=mdb)
+    with pytest.raises(ValueError):
+        validator(5, db=mdb)
+    with pytest.raises(ValueError):
+        validator("asd", db=mdb)
+    with pytest.raises(ValueError):
+        validator([1, 2, 3, 4], db=mdb)
+    with pytest.raises(ValueError):
+        validator(4.5, db=mdb)
+
+
+def test_validate_description():
+    """Confirm that only valid strings are accepted."""
+    validator = validate.VALIDATION_MAPPER["description"]
+    assert validator("Test")
+    with pytest.raises(ValueError):
+        validator(5)
+    with pytest.raises(ValueError):
+        validator(["asd"])
+    with pytest.raises(ValueError):
+        validator(("asd",))
+    with pytest.raises(ValueError):
+        validator(4.5)
+
+
+def test_validate_editors(mdb):
+    """Confirm that only valid users are accepted."""
+    validator = validate.VALIDATION_MAPPER["editors"]
+    test_users = [str(entry['_id']) for entry in mdb["users"].aggregate([{"$sample": {"size": 5}}])]
+    assert validator(test_users, db=mdb)
+    assert validator(test_users[:1], db=mdb)
+    with pytest.raises(ValueError):
+        validator(test_users[0], db=mdb)
+    with pytest.raises(ValueError):
+        validator([str(uuid.uuid4()) for _ in range(4)], db=mdb)
+    with pytest.raises(ValueError):
+        validator(5, db=mdb)
+    with pytest.raises(ValueError):
+        validator("asd", db=mdb)
+    with pytest.raises(ValueError):
+        validator([1, 2, 3, 4], db=mdb)
+    with pytest.raises(ValueError):
+        validator(4.5, db=mdb)
+
+
+def test_validate_generators(mdb):
+    """Confirm that only valid users are accepted."""
+    validator = validate.VALIDATION_MAPPER["editors"]
+    test_users = [str(entry['_id']) for entry in mdb["users"].aggregate([{"$sample": {"size": 5}}])]
+    assert validator(test_users, db=mdb)
+    assert validator(test_users[:1], db=mdb)
+    with pytest.raises(ValueError):
+        validator(test_users[0], db=mdb)
+    with pytest.raises(ValueError):
+        validator([str(uuid.uuid4()) for _ in range(4)], db=mdb)
+    with pytest.raises(ValueError):
+        validator(5, db=mdb)
+    with pytest.raises(ValueError):
+        validator("asd", db=mdb)
+    with pytest.raises(ValueError):
+        validator([1, 2, 3, 4], db=mdb)
+    with pytest.raises(ValueError):
+        validator(4.5, db=mdb)
