26bc8c3dbf33915da048083534b5ec7463a9cb34
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-04-14 11:30:30 +0200

    Add test for secure_description

--- backend/tests/test_utils.py
+++ backend/tests/test_utils.py
@@ -15,3 +15,12 @@ def test_is_email():
     assert not utils.is_email("asd")
     assert not utils.is_email([1, 2, 3, 4])
     assert not utils.is_email(4.5)
+
+
+def test_secure_description():
+    """Confirm that html is escaped."""
+    indata = '# Title *bold* <a href="http://www.example.com">Link</a>'
+    expected = (
+        "# Title *bold* &lt;a href=&quot;http://www.example.com&quot;&gt;Link&lt;/a&gt;"
+    )
+    assert utils.secure_description(indata) == expected
