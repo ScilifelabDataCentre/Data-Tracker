3cfe41a5a120a86c1b489b1c07b47d2647b93f52
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-05-26 15:02:38 +0200

    New wrapper for make_log_new from requests

--- backend/utils.py
+++ backend/utils.py
@@ -573,13 +573,51 @@ def has_permission(permission: str, user_permissions: list):
     return True
 
 
+def req_make_log_new(
+    data_type: str,
+    action: str,
+    comment: str,
+    data: dict,
+) -> bool:
+    """
+    Log a change in the system.
+
+    Wrapper for Flask requests.
+
+    Saves a complete copy of the new object.
+
+    Warning:
+        It is assumed that all values are exactly like in the db,
+        e.g. ``data`` should only contain permitted fields.
+
+    Args:
+        data_type (str): The collection name.
+        action (str): Type of action (add, edit, delete).
+        comment (str): Note about why the change was done
+            (e.g. "Dataset added via addDataset").
+        data (dict): The new data for the entry.
+
+    Returns:
+        bool: Whether the log insertion succeeded.
+    """
+    return make_log_new(
+        db=flask.g.db,
+        data_type=data_type,  # to make singular (e.g. collection|s)
+        action=operation,
+        comment=comment,
+        user_id=flask.g.current_user["_id"],
+        data=data,
+        logger=flask.current_app.logger,
+    )
+
+
 def make_log_new(
-    db: str,
+    db,
     data_type: str,
     action: str,
     comment: str,
     user_id,
-    data,
+    data: dict,
     logger=None,
 ) -> bool:
     """
@@ -710,14 +748,11 @@ def req_commit_to_db(
         if operation == "add":
             data_res["ins_id"] = result.inserted_id
             data = flask.g.db[dbcollection].find_one({"_id": data_res["ins_id"]})
-        log_res = make_log_new(
-            db=flask.g.db,
+        log_res = req_make_log_new(
             data_type=dbcollection[:-1],  # to make singular (e.g. collection|s)
             action=operation,
             comment=comment,
-            user_id=flask.g.current_user["_id"],
             data=data,
-            logger=flask.current_app.logger,
         )
     return CommitResult(data=data_res["ack"], log=log_res, ins_id=data_res["ins_id"])
 
