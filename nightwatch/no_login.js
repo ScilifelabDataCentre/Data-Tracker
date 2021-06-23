0638f9b28421ce56fdff0b4d76a5a8b528787912
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-06-23 14:07:57 +0200

    Drawer tests

--- nightwatch/no_login.js
+++ nightwatch/no_login.js
@@ -1,9 +1,8 @@
 describe('Data Tracker - not logged in', function() {
 
-  before(browser => browser.url('http://localhost:5000/'));
-
   test('Test Basic Details', function (browser) {
     browser
+      .url('http://localhost:5000/')
       .waitForElementVisible('body')
       .assert.titleContains('Data Tracker')
       .assert.elementPresent('.q-header')
@@ -13,27 +12,63 @@ describe('Data Tracker - not logged in', function() {
 
   test('Check First Page Content', function (browser) {
     browser
-      .assert.containsText('#index-q-card-datasets', 'Datasets')
-      .assert.containsText('#index-q-card-collections', 'Collections')
-      .assert.not.containsText('.q-page', 'Orders');
+      .url('http://localhost:5000/')
+      .waitForElementVisible('body')
+      .assert.elementPresent('#index-card-datasets')
+      .assert.elementPresent('#index-card-collections')
+      .assert.not.elementPresent('#index-card-orders')
+      .click('#index-card-datasets')
+      .assert.urlEquals('http://localhost:5000/datasets/')
+      .url('http://localhost:5000/')
+      .waitForElementVisible('body')
+      .click('#index-card-collections')
+      .assert.urlEquals('http://localhost:5000/collections/');
   });
 
-  test('Check Drawer Content', function (browser) {
+  test('Check Drawer Content and Navigation', function (browser) {
     browser
-      .assert.not.containsText('.q-drawer', 'Orders')
-      .assert.containsText('.q-drawer', 'Datasets')
-      .assert.containsText('.q-drawer', 'Collections')
-      .assert.not.containsText('.q-drawer', 'Users')
-      .assert.containsText('.q-drawer', 'About the Data Tracker')
-      .assert.containsText('.q-drawer', 'User Guide')
-      .assert.not.containsText('.q-drawer', 'Current User')
-      .assert.containsText('.q-drawer', 'Log In')
-      .assert.not.containsText('.q-drawer', 'Log Out')
-  });
+      .url('http://localhost:5000/')
+      .waitForElementVisible('body')
 
+      .click("#header-toggle-drawer")
+      .pause(500)
+      .assert.cssClassPresent('.q-drawer', 'q-layout--prevent-focus')
+      .click("#header-toggle-drawer")
+      .pause(500)
+      .assert.not.cssClassPresent('.q-drawer', 'q-layout--prevent-focus')
+
+      .assert.not.elementPresent('#drawer-entry-orders')
+
+      .assert.elementPresent('#drawer-entry-datasets')
+      .click('#drawer-entry-datasets')
+      .assert.urlEquals('http://localhost:5000/datasets/')
+
+      .assert.elementPresent('#drawer-entry-collections')
+      .click('#drawer-entry-collections')
+      .assert.urlEquals('http://localhost:5000/collections/')
+
+      .assert.not.elementPresent('#drawer-entry-users')
+
+      .assert.elementPresent('#drawer-entry-about')
+      .click('#drawer-entry-about')
+      .assert.urlEquals('http://localhost:5000/about')
+
+      .assert.elementPresent('#drawer-entry-guide')
+      .click('#drawer-entry-guide')
+      .assert.urlEquals('http://localhost:5000/guide')
+
+      .assert.not.elementPresent('#drawer-entry-current-user')
+
+      .assert.elementPresent('#drawer-entry-log-in')
+      .click('#drawer-entry-log-in')
+      .assert.urlEquals('http://localhost:5000/login')
+
+      .assert.not.elementPresent('#drawer-entry-log-out');
+  });
+  
   test('Test Dataset Info Page', function (browser) {
     browser
-      .click('#index-q-card-datasets')
+      .url('http://localhost:5000/datasets')
       .waitForElementVisible('.q-table--grid')
       .setValue('input[type=search]', 'Frontend Test Dataset')
       .waitForElementVisible('#entry-79a755f1-69b0-4734-9977-ac945c4c51c1')
