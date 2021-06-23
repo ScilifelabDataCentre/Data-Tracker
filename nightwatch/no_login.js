3f6402c2031bbe38838870d58dc9f03367f4898f
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-06-23 12:41:09 +0200

    Dataset about page tests

--- nightwatch/no_login.js
+++ nightwatch/no_login.js
@@ -31,15 +31,51 @@ describe('Data Tracker - not logged in', function() {
       .assert.not.containsText('.q-drawer', 'Log Out')
   });
 
-  test('Test Datasets', function (browser) {
+  test('Test Dataset Info Page', function (browser) {
     browser
       .click('#index-q-card-datasets')
       .waitForElementVisible('.q-table--grid')
-      .setValue('input[type=search]', 'Dataset 500')
-      .pause(1000)
-      .assert.containsText('.q-table__grid-content', 'Dataset 500 Title')
-      .click('.q-table__grid-content .q-card')
-      .pause(10000)
+      .setValue('input[type=search]', 'Frontend Test Dataset')
+      .waitForElementVisible('#entry-79a755f1-69b0-4734-9977-ac945c4c51c1')
+      .assert.containsText('#entry-79a755f1-69b0-4734-9977-ac945c4c51c1', 'Frontend Test Dataset')
+      .click('#entry-79a755f1-69b0-4734-9977-ac945c4c51c1')
+      .waitForElementVisible('.q-tab-panel h1')
+      .assert.containsText('#entry-about-title-text', 'Frontend Test Dataset')
+      .assert.containsText('#entry-about-title-identifier', '79a755f1-69b0-4734-9977-ac945c4c51c1')
+      .assert.containsText('#entry-about-tags', 'Frontend')
+      .assert.containsText('#entry-about-tags', 'Test')
+      .assert.containsText('#entry-about-properties .q-chip__content span', 'Type')
+      .assert.containsText('#entry-about-properties .q-chip__content', 'Frontend Test Entry')
+      .assert.containsText('#entry-about-description', 'A dataset added for frontend tests')
+
+      .assert.containsText('#entry-about-authors-0', 'Frontend Author')
+      .assert.not.containsText('#entry-about-authors-0', 'author@frontend.dev')
+      .click('#entry-about-authors-0 .q-focusable')
+      .assert.containsText('#entry-about-authors-0', 'author@frontend.dev')
+      .assert.containsText('#entry-about-authors-0', 'Frontend Test University')
+      .assert.containsText('#entry-about-authors-0', 'https://www.example.com/frontend_author')
+      .click('#entry-about-authors-0 .q-focusable')
+      .assert.not.containsText('#entry-about-authors-0', 'author@frontend.dev')
+
+      .assert.containsText('#entry-about-generators', 'Frontend Generator')
+      .assert.not.containsText('#entry-about-generators', 'generator@frontend.dev')
+      .click('#entry-about-generators-0 .q-focusable')
+      .assert.containsText('#entry-about-generators', 'generator@frontend.dev')
+      .assert.containsText('#entry-about-generators', 'Frontend Test University')
+      .assert.containsText('#entry-about-generators', 'https://www.example.com/frontend_generator')
+      .click('#entry-about-generators-0 .q-focusable')
+      .assert.not.containsText('#entry-about-generators', 'generator@frontend.dev')
+
+      .assert.containsText('#entry-about-organisation', 'Frontend Organisation')
+      .assert.not.containsText('#entry-about-organisation', 'organisation@frontend.dev')
+      .click('#entry-about-organisation .q-focusable')
+      .assert.containsText('#entry-about-organisation', 'organisation@frontend.dev')
+      .assert.containsText('#entry-about-organisation', 'Frontend Test University')
+      .assert.containsText('#entry-about-organisation', 'https://www.example.com/frontend_organisation')
+      .click('#entry-about-organisation .q-focusable')
+      .assert.not.containsText('#entry-about-organisation', 'organisation@frontend.dev')
+
+      .assert.not.elementPresent('#entry-about-editors')
   });
   
   after(browser => browser.end());
