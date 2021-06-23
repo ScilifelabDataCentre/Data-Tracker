7d0ba237e92ded477a35e7d8acdde8fd63500853
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-06-23 13:32:43 +0200

    Flatten and add ids

--- frontend/src/components/MainDrawerContent.vue
+++ frontend/src/components/MainDrawerContent.vue
@@ -3,34 +3,36 @@
   <q-item-label header> Data </q-item-label>
 
   <NavEntry v-if="currentUser.permissions.includes('DATA_EDIT')"
+            id="drawer-entry-orders"
             v-bind="orderLink" />
 
-  <NavEntry v-for="link in dataLinks"
-            :key="link.title"
-            v-bind="link" />
+  <NavEntry v-bind="datasetLink"
+            id="drawer-entry-datasets" />
+  <NavEntry v-bind="collectionLink"
+            id="drawer-entry-collections" />
 
   <div v-if="currentUser.permissions.includes('USER_MANAGEMENT')">
     <q-item-label header> Admin </q-item-label>
-    <NavEntry v-bind="adminUserLink" />
+    <NavEntry v-bind="adminUserLink"
+              id="drawer-entry-users" />
   </div>
 
   <div>
     <q-item-label header> Other </q-item-label>
-    <NavEntry v-for="link in otherLinks"
-              :key="link.title"
-              v-bind="link"/>
+    <NavEntry v-bind="aboutLink"
+              id="drawer-entry-about" />
+    <NavEntry v-bind="guideLink"
+              id="drawer-entry-guide" />
   </div>
 
   <q-item-label header> User </q-item-label>
   <div v-if="currentUser.name !== ''">
-    <NavEntry v-for="link in userLinks"
-              :key="link.title"
-              v-bind="link" />
+    <NavEntry v-bind="currentUserLink"
+              id="drawer-entry-current"/>
     <q-item clickable
-            tag="a"
+            id="drawer-entry-log-out"
             @click="logOut">
-      <q-item-section v-if="logoutLink.icon"
-                      avatar>
+      <q-item-section avatar>
         <q-icon :name="logoutLink.icon" />
       </q-item-section>
 
@@ -43,9 +45,8 @@
     </q-item>
   </div>
   <div v-else>
-    <NavEntry v-for="link in loginLinks"
-              :key="link.title"
-              v-bind="link" />
+    <NavEntry v-bind="loginLink"
+              id="drawer-entry-log-in"/>
   </div>
 
 </q-list>
@@ -88,60 +89,54 @@ export default {
         link: { 'name': 'User Manager'}
       },
 
-      dataLinks: [
-        {
-          title: 'Datasets',
-          caption: 'Data deliveries',
-          icon: 'fas fa-chart-area',
-          link:  { 'name': 'Dataset Browser'}
-        },
-        {
-          title: 'Collections',
-          caption: 'Collections of datasets',
-          icon: 'fas fa-layer-group',
-          link:  { 'name': 'Collection Browser'}
-        }
-      ],
+      datasetLink: {
+        title: 'Datasets',
+        caption: 'Data deliveries',
+        icon: 'fas fa-chart-area',
+        link:  { 'name': 'Dataset Browser'}
+      },
+
+      collectionLink:  {
+        title: 'Collections',
+        caption: 'Collections of datasets',
+        icon: 'fas fa-layer-group',
+        link:  { 'name': 'Collection Browser'}
+      },
       
-      userLinks: [
-        {
-          title: 'Current User',
-          caption: 'About the current user',
-          icon: 'person',
-          link: { 'name': 'About Current User'}
-        },
-      ],
+      currentUserLink: {
+        title: 'Current User',
+        caption: 'About the current user',
+        icon: 'person',
+        link: { 'name': 'About Current User'}
+      },
       
       logoutLink: {
         title: 'Log Out',
         caption: 'Log out the current user',
         icon: 'exit_to_app',
-        link: '/api/v1/logout/'
+        link: ''
       },
 
-      loginLinks: [
-        {
-          title: 'Log In',
-          caption: 'Log in using OpenID or API Key',
-          icon: 'login',
-          link: { 'name': 'Login'}
-        },
-      ],
+      loginLink: {
+        title: 'Log In',
+        caption: 'Log in using OpenID or API Key',
+        icon: 'login',
+        link: { 'name': 'Login'}
+      },
 
-      otherLinks: [
-        {
-          title: 'About',
-          caption: 'About the Data Tracker',
-          icon: 'info',
-          link:  { 'name': 'About'}
-        },
-        {
-          title: 'User Guide',
-          caption: 'How to use the Data Tracker',
-          icon: 'fas fa-map-signs',
-          link:  { 'name': 'User Guide'}
-        }
-      ]
+      aboutLink: {
+        title: 'About',
+        caption: 'About the Data Tracker',
+        icon: 'info',
+        link:  { 'name': 'About'}
+      },
+
+      guideLink: {
+        title: 'User Guide',
+        caption: 'How to use the Data Tracker',
+        icon: 'fas fa-map-signs',
+        link:  { 'name': 'User Guide'}
+      }
     }
   },
 
