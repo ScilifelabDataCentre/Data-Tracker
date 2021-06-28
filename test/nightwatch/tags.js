dfaae1a7a714a9e7235849cb368292b05d7d7fa6
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-06-28 22:31:11 +0200

    Split into parts

--- test/nightwatch/tags.js
+++ test/nightwatch/tags.js
@@ -2,7 +2,7 @@ describe('Data Tracker - Tag Editor', function() {
 
   before((browser) => browser.url('http://localhost:5000/api/v1/developer/login/generator::frontend'))
 
-  test('Test tag editor', function (browser) {
+  test('Open tag editor', function (browser) {
     browser
       .url('http://localhost:5000/orders')
       .waitForElementVisible('#entry-browser-add')
@@ -10,20 +10,29 @@ describe('Data Tracker - Tag Editor', function() {
       .waitForElementVisible('.q-list')
       .assert.not.visible('#entry-edit-tags input')
       .click('#entry-edit-tags .q-item--clickable')
+  });
+
+  test('Test adding tags', function (browser) {
+    browser
       .setInputValue('#entry-edit-tags input', 'New Tag1')
       .assert.elementPresent('#entry-edit-tags .q-field__append .fa-plus')
       .assert.elementPresent('#entry-edit-tags .q-field__messages:empty')
       .keys('\uE006')
       .assert.containsText('#entry-edit-tags .q-chip', 'New Tag1')
-    
       .setInputValue('#entry-edit-tags input', 'New Tag2')
       .click('#entry-edit-tags button')
       .assert.containsText('#entry-edit-tags .flex .q-chip:nth-of-type(2)', 'New Tag2')
-    
+  });
+  
+  test('Test removing tags', function (browser) {
+    browser
       .click('#entry-edit-tags .q-chip__icon--remove')
       .assert.not.containsText('#entry-edit-tags .q-chip', 'New Tag1')
       .assert.containsText('#entry-edit-tags .q-chip', 'New Tag2')
-    
+  });
+
+  test('Test tag style evaluation', function (browser) {
+    browser  
       .setInputValue('#entry-edit-tags input', '')
       .assert.not.containsText('#entry-edit-tags .q-field__append', 'error')
       .assert.not.visible('#entry-edit-tags .q-field__append .fa-plus')
@@ -41,7 +50,10 @@ describe('Data Tracker - Tag Editor', function() {
       .setInputValue('#entry-edit-tags input', 'ba')
       .assert.not.elementPresent('#entry-edit-tags .q-field__messages:empty')
       .assert.containsText('#entry-edit-tags .q-field__append .material-icons', 'error')
-
+  });
+  
+  test('Close tag editor', function (browser) {
+    browser
       .click('#entry-edit-tags .q-item--clickable')
       .assert.not.visible('#entry-edit-tags input')
   });
