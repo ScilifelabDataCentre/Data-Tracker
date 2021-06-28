9d0d1938eff3c4be106b5713132fc05325ea3077
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-06-28 22:38:13 +0200

    Clean up until adding properties

--- test/nightwatch/order.js
+++ test/nightwatch/order.js
@@ -34,7 +34,6 @@ describe('Data Tracker - Orders', function() {
       .setInputValue('#entry-edit-title', 'Order from frontend test')
       .setInputValue('#entry-edit-description', 'An order created during a frontend test run')
 
-      .assert.not.visible('#entry-edit-tags input')
       .click('#entry-edit-tags .q-item--clickable')
       .setInputValue('#entry-edit-tags input', 'New Tag1')
       .keys('\uE006')
@@ -42,46 +41,40 @@ describe('Data Tracker - Orders', function() {
       .keys('\uE006')
       .click('#entry-edit-tags .q-item--clickable')
 
-
-      .assert.not.visible('#entry-edit-properties input')
       .click('#entry-edit-properties .q-item--clickable')
       .setInputValue('#entry-edit-properties input', 'Key1')
       .keys('\uE006')
       .setInputValue('#entry-edit-properties input', 'Key2')
       .keys('\uE006')
-      .assert.containsText('#entry-edit-properties .q-field__label', 'Key1')
-      .setInputValue('#entry-edit-properties .q-list input', 'Value1')
-      .assert.containsText('#entry-edit-properties .flex .q-chip:nth-of-type(2)', 'New Tag2')
-      .click('#entry-edit-properties .q-chip__icon--remove')
-      .assert.not.containsText('#entry-edit-properties .q-chip', 'New Tag1')
-      .assert.containsText('#entry-edit-properties .q-chip', 'New Tag2')
+      .setInputValue('#entry-edit-properties .q-list input[aria-label=Key1]', 'Value1')
+      .setInputValue('#entry-edit-properties .q-list input[aria-label=Key2]', 'Value2')
       .click('#entry-edit-properties .q-item--clickable')
       .assert.not.visible('#entry-edit-properties input')
   });
 
   test('Test editing order', function (browser) {
     browser
-      .url('http://localhost:5000/collections')
+      .url('http://localhost:5000/orders')
       .waitForElementVisible('.q-table--grid')
       .click('#entry-browser-add')
-      .assert.urlEquals('http://localhost:5000/collections/add')
+      .assert.urlEquals('http://localhost:5000/orders/add')
   });
 
 
-  test('Test adding collection', function (browser) {
+  test('Test adding order', function (browser) {
     browser
-      .url('http://localhost:5000/collections')
+      .url('http://localhost:5000/orders')
       .waitForElementVisible('.q-table--grid')
       .click('#entry-browser-add')
-      .assert.urlEquals('http://localhost:5000/collections/add')
+      .assert.urlEquals('http://localhost:5000/orders/add')
   });
 
-  test('Test editing collection', function (browser) {
+  test('Test editing order', function (browser) {
     browser
-      .url('http://localhost:5000/collections')
+      .url('http://localhost:5000/orders')
       .waitForElementVisible('.q-table--grid')
       .click('#entry-browser-add')
-      .assert.urlEquals('http://localhost:5000/collections/add')
+      .assert.urlEquals('http://localhost:5000/orders/add')
   });
 
   after(browser => browser.end());
