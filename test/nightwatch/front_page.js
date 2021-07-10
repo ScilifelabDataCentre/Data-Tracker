978f518ff91e0d7689b63d18fc280b8ef283c294
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-07-10 07:54:11 +0200

    Add test for 'goto entry'

--- test/nightwatch/front_page.js
+++ test/nightwatch/front_page.js
@@ -1,5 +1,4 @@
 describe('Data Tracker - Front Page', function() {
-
   test('Test basic details', function (browser) {
     browser
       .url('http://localhost:5000/')
@@ -45,6 +44,54 @@ describe('Data Tracker - Front Page', function() {
       .assert.elementPresent('#index-card-collections')
       .assert.elementPresent('#index-card-orders')
   });
+
+  test('Go to entry', function (browser) {
+    browser
+      .url('http://localhost:5000/api/v1/logout')
+      .url('http://localhost:5000/')
+      .waitForElementVisible('.q-page input')
+      .setInputValue('.q-page input', 'd-79a755f1-69b0-4734-9977-ac945c4c51c1')
+      .assert.not.containsText('.q-field .q-field__messages', 'Identifiers start with o-, d-, or c-')
+      .keys('\uE006')
+      .assert.urlEquals('http://localhost:5000/datasets/d-79a755f1-69b0-4734-9977-ac945c4c51c1')
+
+      .url('http://localhost:5000/')
+      .waitForElementVisible('.q-page input')
+      .setInputValue('.q-page input', 'c-21c8ecd1-9908-462f-ba84-3ca399074b36')
+      .assert.not.containsText('.q-field .q-field__messages', 'Identifiers start with o-, d-, or c-')
+      .click('.q-page .q-btn')
+      .assert.urlEquals('http://localhost:5000/collections/c-21c8ecd1-9908-462f-ba84-3ca399074b36')
+
+      .url('http://localhost:5000/')
+      .waitForElementVisible('.q-page input')
+      .setInputValue('.q-page input', 'o-d4467732-8ddd-43a6-a904-5b7376f60e5c')
+      .assert.not.containsText('.q-field .q-field__messages', 'Identifiers start with o-, d-, or c-')
+      .click('.q-page .q-btn')
+      .assert.urlEquals('http://localhost:5000/forbidden/')
+
+      .url('http://localhost:5000/api/v1/developer/login/editor::frontend')
+      .url('http://localhost:5000/')
+      .waitForElementVisible('.q-page input')
+      .setInputValue('.q-page input', 'o-d4467732-8ddd-43a6-a904-5b7376f60e5c')
+      .assert.not.containsText('.q-field .q-field__messages', 'Identifiers start with o-, d-, or c-')
+      .click('.q-page .q-btn')
+      .assert.urlEquals('http://localhost:5000/orders/o-d4467732-8ddd-43a6-a904-5b7376f60e5c')
+
+      .url('http://localhost:5000/')
+      .waitForElementVisible('.q-page input')
+      .setInputValue('.q-page input', 'o-d446773')
+      .assert.not.containsText('.q-field .q-field__messages', 'Identifiers start with o-, d-, or c-')
+      .click('.q-page .q-btn')
+      .assert.urlEquals('http://localhost:5000/orders/o-d446773')
+
+      .url('http://localhost:5000/')
+      .waitForElementVisible('.q-page input')
+      .setInputValue('.q-page input', 'a-d446773')
+      .assert.containsText('.q-field .q-field__messages', 'Identifiers start with o-, d-, or c-')
+      .click('.q-page .q-btn')
+      .assert.urlEquals('http://localhost:5000/')
+  });
+
   
   after(browser => browser.end());
 });
